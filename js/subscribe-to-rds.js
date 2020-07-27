d688ae13be18354fd59fe20d8c13fb2e9802bee7
takshch <tkcphp@gmail.com>
2020-07-27 18:56:16 +0530

    Dynamic Scripts & CSS, DOM Manipulation

--- js/subscribe-to-rds.js
+++ js/subscribe-to-rds.js
@@ -1,86 +1,166 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyDw-ey9ur4n2UGDrqORomNYkjsC_xShAyo",
-    authDomain: "realdevsquad-ffd38.firebaseapp.com",
-    databaseURL: "https://realdevsquad-ffd38.firebaseio.com",
-    projectId: "realdevsquad-ffd38",
-    storageBucket: "realdevsquad-ffd38.appspot.com",
-    messagingSenderId: "782955653404",
-    appId: "1:782955653404:web:d9703542121f51bc71f2d9",
-    measurementId: "G-3H138QV9KE"
-};
 
-// Initialize Firebase
+function SubscribeRDS(){
+	this.firebase = null;
+	
+	this.srcArray  = ["https://www.gstatic.com/firebasejs/7.17.1/firebase-database.js",
+	// "https://www.gstatic.com/firebasejs/7.17.1/firebase.js",
+	"https://www.gstatic.com/firebasejs/7.17.1/firebase-app.js",
+	"https://www.gstatic.com/firebasejs/7.17.1/firebase-analytics.js",
+	"https://www.gstatic.com/firebasejs/7.17.1/firebase-auth.js",
+	"https://www.gstatic.com/firebasejs/7.17.1/firebase-firestore.js"];
+	
+	this.firebaseConfig = {
+		apiKey: "AIzaSyDw-ey9ur4n2UGDrqORomNYkjsC_xShAyo",
+		authDomain: "realdevsquad-ffd38.firebaseapp.com",
+		databaseURL: "https://realdevsquad-ffd38.firebaseio.com",
+		projectId: "realdevsquad-ffd38",
+		storageBucket: "realdevsquad-ffd38.appspot.com",
+		messagingSenderId: "782955653404",
+		appId: "1:782955653404:web:d9703542121f51bc71f2d9",
+		measurementId: "G-3H138QV9KE"
+	};
+	
+	
+	this.initialize = function(){
+		document.addEventListener("DOMContentLoaded",function(e){
+			this.addSubscribeLink();
+			this.addFirebaseScripts(this.srcArray);
+			this.addSubscribeElement();
+			this.subscribeListener();
+		}.bind(this));
+	}
+	
+	
+	this.addFirebaseScripts = function(srcArray){
+		let scriptsNode = [];
+		let headNode = document.getElementsByTagName("head")[0];
+		let status = false;
+		srcArray.forEach(function(valueOfSrc){
+			let script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.src = valueOfSrc;
+			scriptsNode.push(script);
+			headNode.appendChild(script);
+		});	
+		
+		scriptsNode[scriptsNode.length-1].onload = ()=>{
+			this.firebaseInitialize();
+		}
+	}
+	
+	this.addSubscribeLink = function(){
+		let headNode = document.getElementsByTagName("head")[0];
+		let linkNode = document.createElement('link');
+		linkNode.rel = "stylesheet";
+		linkNode.href = "./css/subscribe.css";
+		headNode.appendChild(linkNode);
+	}
+	
+	this.firebaseDatabase = null;
 
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+	this.firebaseInitialize = function(){
+		this.firebase = firebase.initializeApp(this.firebaseConfig);
+		this.firebase.analytics();
+		this.firebaseDatabase = this.firebase.database();
+	}
 
-var database = firebase.database();
+	this.addSubscribeElement = function(){
+		let footerNode = document.getElementsByTagName("footer")[0];
+		let subscribeNode = document.createElement("div");
+		subscribeNode.classList.add("subscribe");
+		subscribeNode.innerHTML = 
+		`<div class="subscribe__heading">
+			<span>Want to stay updated?</span>
+			<span>Subscribe to  our Newsletter</span>
+		</div>
+		<div class="subscribe__inner">
+			<input class="subscribe__input" placeholder="Enter Email address" />
+			<button class="subscribe__button">Subscribe</button>
+		</div>
+		<div class="subscribe_status"></div>`;
 
-/** function for adding email to database
-	1: check the users exits or not
-	2: if not then add email to the database
-	3: if yes, assign string to message
-	var nextId is used for giving index to next entry.
- */
-async function addEmail(email,callback){
-	var message = null;
-	await database.ref().child("users").orderByChild("email").equalTo(email).once("value").
-		then(async function(snapshot) {
-				console.log(`Exits: ${snapshot.exists()}`);
-				if (snapshot.exists()) {
-					message="You have already subscribed to our newsletter.";
-				}else{
-					let nextId = null;
-					await database.ref('next/id').once('value',function(snapshot) {
-						nextId  = snapshot.val(); 
-						database.ref("next").set({
-							id: nextId + 1
+		footerNode.prepend(subscribeNode);
+	}
+
+
+	this.subscribeListener = function(){
+		let subscribe_button = document.getElementsByClassName("subscribe__button")[0]; //subscribe button Node
+		/** onClick Event Listener for subscribe button */
+		subscribe_button.addEventListener("click",function(event){ 
+			// Note: Async Await Function Callback
+			let subscribe_email = document.getElementsByClassName("subscribe__input")[0].value;
+			var message = null;
+			let fn_this = this;
+			console.log(subscribe_email);
+			if(ValidateEmail(subscribe_email)){
+				try{
+					this.firebaseDatabase.ref().child("users").orderByChild("email").equalTo(subscribe_email).
+					once("value",function(snapshot) {
+								console.log(`Exits: ${snapshot.exists()}`);
+								if (snapshot.exists()) {
+									fn_this.message="You have already subscribed to our newsletter.";
+									fn_this.showMessage();
+								}else{
+									let nextId = null;
+									fn_this.firebaseDatabase.ref('next/id').once('value',function(snapshot) {
+										nextId  = snapshot.val(); 
+										fn_this.firebaseDatabase.ref("next").set({
+											id: nextId + 1
+										});
+										fn_this.firebaseDatabase.ref('users/'+nextId).set({
+											email: subscribe_email
+										});
+										fn_this.message="You have subscribed to our newsletter.";
+										fn_this.showMessage();
+									});
+							}
 						});
-						database.ref('users/'+nextId).set({
-							email
-						});
-						message="You have subscribed to our newsletter.";
-				});
+				}catch(err){
+					this.message = "Something is wrong";
+					console.log(`addEmail fn 
+						Error : ${err}`);
+				}
+				console.log(`Message : ${message}`);
+				this.showMessage();
+			}else{
+				this.message = "Please enter valid email address";
+				this.showMessage()
+				alert(this.message); //can be removed if not necessary
+				
 			}
-		}).catch((err)=>{
-		message = "Something is wrong";
-		console.log(`addEmail fn 
-			Error : ${err}`);
-	});
-	return message;
+
+
+		}.bind(this));
+	}
+
+	this.message = null;
+	
+	this.showMessage = function(){
+		let subscribe_status_node = document.getElementsByClassName("subscribe_status")[0];
+		subscribe_status_node.innerText = this.message; 
+	}
+
+
+
+	this.initialize();
 }
 
 
-var subscribe_button = document.getElementsByClassName("subscribe__button")[0]; //subscribe button Node
-/** onClick Event Listener for subscribe button */
-subscribe_button.addEventListener("click",async function(event){ 
-	// Note: Async Await Function Callback
-	let subscribe_email = document.getElementsByClassName("subscribe__input")[0].value;
-	let subscribe_status_node = document.getElementsByClassName("subscribe_status")[0];
-	let message = null;
-	console.log(subscribe_email);
-	if(ValidateEmail(subscribe_email)){
-		message = await addEmail(subscribe_email);
-		console.log(`Message : ${message}`);
-		subscribe_status_node.innerText = message;
-	}else{
-		message = "Please enter valid email address";
-		alert(message); //can be removed if not necessary
-		subscribe_status_node.innerText = message;
-	}
-});
 
 /** check email is valid or not */
 function ValidateEmail(mail) 
 {
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
-  {
-    return true;
-  }else{
-    return false;
-  }
+	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+	{
+	return true;
+	}else{
+	return false;
+	}
 }
 
+
+
+new SubscribeRDS();
 
 
 

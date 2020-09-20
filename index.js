e579ab18dd567f5f1d19f70ac1d6c049289f1600
Swaraj Rajpure <swarajrajpure@gmail.com>
2020-09-20 22:54:10 +0530

    mocked response, showed username

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-fetch(url)
-.then(res => res.json())
-.then(res => {
-    console.log(first_name);
-})
+const userLogin = document.querySelector("#user-login");
+const loginBtn = document.querySelector('.btn-login');
+
+const setName = (name) => {
+    userLogin.innerHTML = `Hello, ${name}!`;
+}
+
+const fetchData = () => {
+    fetch('./data.json')
+    .then(res => res.json())
+    .then(res => { 
+        setTimeout(() => { 
+            setName(res.first_name) 
+        }
+        ,2000);
+    })
+}
+
+fetchData();
+
+

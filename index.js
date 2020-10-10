8b4e071081d4e63424f066656e996140d3c03bb9
Swaraj Rajpure <swarajrajpure@gmail.com>
2020-10-10 09:48:42 +0530

    requested changes done

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const userLogin = document.querySelector("#user-login");
 
-const setName = (name) => {
+function setNameIfFound(name){
     if (name) {
         userLogin.innerHTML = `Hello, ${name}!`;    
     }
-}
+};
 
 const fetchData = () => {
     fetch('https://staging-api.realdevsquad.com/users/self')
     .then(res => res.json())
     .then(res => {
-        setName(res.first_name)
-    })
-}
+        setNameIfFound(res.first_name)
+    });
+};
 
 fetchData();
-
-

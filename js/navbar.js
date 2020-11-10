46fac012c6aa0963a18b30fd0754891d92cc7158
Akanksha Dharkar <adharkar@linkedin.com>
2020-11-10 22:02:36 +0530

    Making navbar responsive

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+function changeCSS() {
+  const elementToManipulate = document.getElementById("topNavBar");
+  if (elementToManipulate.className === "topnav") {
+    elementToManipulate.className += " responsive";
+  } else {
+    elementToManipulate.className = "topnav";
+  }
+}

7d68a1b7d2e5018b179662e0cbb8b2195614b26b
Akanksha Dharkar <adharkar@linkedin.com>
2021-01-10 15:33:01 +0530

    Fixing margin on top of nav-bar #77

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
 function changeCSS() {
   const elementToManipulate = document.getElementById("topNavBar");
   if (elementToManipulate.className === "topnav") {
-    elementToManipulate.className += " responsive";
+    elementToManipulate.className += " " + "responsive";
   } else {
     elementToManipulate.className = "topnav";
   }

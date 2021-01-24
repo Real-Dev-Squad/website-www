4e35708c0294e9ef597a16e6aaf116a6a0213f13
Ankush Dharkar <ankushdharkar@users.noreply.github.com>
2021-01-24 11:10:31 -0800

    Redirect to signup form if incompleteUserDetails (#86)
    * Redirect to user form if incomplete use #65
    
    * Send user to homepage on API error or existing user

---
+++ js/goto.js
@@ -0,0 +1,27 @@
+function redirectUserToPage (page) {
+  const finalPage = (page == 'signup')
+    ? 'https://my.realdevsquad.com/signup'
+    : 'https://realdevsquad.com';
+  
+  window.location.href = finalPage;
+}
+
+function showSignupFormIfIncomplete () {
+  fetch('https://staging-api.realdevsquad.com/users/self', {
+      headers: {'content-type': 'application/json'},
+      credentials: 'include'
+  })
+  .then(res => res.json())
+  .then(data => {
+      if(data.incompleteUserDetails) {
+        redirectUserToPage('signup');
+      }
+      else {
+        redirectUserToPage('home');
+      }
+  }).catch(e => {
+    redirectUserToPage('home');
+  });
+}
+
+showSignupFormIfIncomplete();

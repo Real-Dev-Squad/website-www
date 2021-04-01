2e7f8984224f889e3904e8fdc1ca031067839be8
Swaraj Rajpure <56217868+swarajpure@users.noreply.github.com>
2021-04-01 18:20:03 +0530

    Sync main with develop (#107)
    * Setting up of prettier CLI (#99)
    
    * feature/hook-format using bare minimum required (#104)
    
    Co-authored-by: Venkataswamy, Kanakaraj (kv760r) <kv760r@att.com>
    
    * [feature/hook-format] updated README.md about pre-commit hook (#106)
    
    Co-authored-by: Venkataswamy, Kanakaraj (kv760r) <kv760r@att.com>
    
    * [feature/hook-format] updated README.md about pre-commit hook (#105)
    
    Co-authored-by: Venkataswamy, Kanakaraj (kv760r) <kv760r@att.com>
    
    Co-authored-by: Tanya Dixit <61655940+TD-17@users.noreply.github.com>
    Co-authored-by: rajakvk <rajakvk@gmail.com>
    Co-authored-by: Venkataswamy, Kanakaraj (kv760r) <kv760r@att.com>

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 [![Netlify Status](https://api.netlify.com/api/v1/badges/f178400b-1528-4bae-b454-8cddfd2fde0b/deploy-status)](https://app.netlify.com/sites/realdevsquad/deploys)
 [![Better Uptime Badge](https://betteruptime.com/status-badges/v1/monitor/5hut.svg)](https://betteruptime.com/?utm_source=status_badge)
 
@@ -8,12 +7,25 @@ Welcome to Real Dev Squad
 
 Code deployed on: https://realdevsquad.netlify.app/, then served to our domain
 
-## CSS Structure    
-### Global CSS  
-   - `main.css`
-      - This css file will contain common css pertaining to all pages that can be used throughout the website.   
-      - For example, a class with name success, when used anywhere throughout the website will make the color of the text green.  
-### Page specific CSS 
-   - For example, events page css goes inside `events.css`.
-   - Also, in future if we add new components, we will keep component specific css like `navbar.css`, `footer.css`, `header.css` etc. 
-   - For mobile responsive css, we can have a folder with multiple css files catering to different css pertaining to different resolutions. 
+## Running the project
+
+Run `npm install` after opening the repository in your editor. This will install all the necessary packages for the project which are mentioned in the `pacakage.json` file of the project.
+
+## Scripts used in project
+
+- `npm run check` checks the formatting of all the files in the project. This script runs in a pre-commit hook. It will warn about the improper formatting present in the file.
+- `npm run fix` can be run for fixing the formatting before commiting your changes or to solve the formatting errors one may get while commiting the changes.
+
+## CSS Structure
+
+### Global CSS
+
+- `main.css`
+  - This css file will contain common css pertaining to all pages that can be used throughout the website.
+  - For example, a class with name success, when used anywhere throughout the website will make the color of the text green.
+
+### Page specific CSS
+
+- For example, events page css goes inside `events.css`.
+- Also, in future if we add new components, we will keep component specific css like `navbar.css`, `footer.css`, `header.css` etc.
+- For mobile responsive css, we can have a folder with multiple css files catering to different css pertaining to different resolutions.
--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
-function setUserGreeting(username, firstName){
-    if (username) {
-        const userLoginEl = document.querySelector('.btn-login');
-        
-        const greetingEl = document.querySelector('.user-greet');
-        const msgGreetMsgEl = document.querySelector('.user-greet-msg');
-        const userImgEl = document.querySelector('.user-profile-pic');
+function setUserGreeting(username, firstName) {
+  if (username) {
+    const userLoginEl = document.querySelector(".btn-login");
 
-        const greetMsg = `Hello, ${firstName}!`;
-        msgGreetMsgEl.innerText = greetMsg;
-        const userImgURL = `https://raw.githubusercontent.com/Real-Dev-Squad/website-static/main/members/${username}/img.png`
-        userImgEl.src = userImgURL;
+    const greetingEl = document.querySelector(".user-greet");
+    const msgGreetMsgEl = document.querySelector(".user-greet-msg");
+    const userImgEl = document.querySelector(".user-profile-pic");
 
-        greetingEl.style.display = 'block';
-        userLoginEl.style.display = 'none';
-    }
-};
+    const greetMsg = `Hello, ${firstName}!`;
+    msgGreetMsgEl.innerText = greetMsg;
+    const userImgURL = `https://raw.githubusercontent.com/Real-Dev-Squad/website-static/main/members/${username}/img.png`;
+    userImgEl.src = userImgURL;
 
-function fetchData () {
-    fetch('https://api.realdevsquad.com/users/self', {
-        headers: {'content-type': 'application/json'},
-        credentials: 'include'
-    })
-    .then(res => res.json())
-    .then(res => {
-        if(res.incompleteUserDetails){
-            return window.location.replace('https://my.realdevsquad.com/signup')
-        }
-        setUserGreeting(res.username, res.first_name);
+    greetingEl.style.display = "block";
+    userLoginEl.style.display = "none";
+  }
+}
+
+function fetchData() {
+  fetch("https://api.realdevsquad.com/users/self", {
+    headers: { "content-type": "application/json" },
+    credentials: "include",
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.incompleteUserDetails) {
+        return window.location.replace("https://my.realdevsquad.com/signup");
+      }
+      setUserGreeting(res.username, res.first_name);
     });
-};
+}
 
-window.addEventListener('DOMContentLoaded', fetchData);
+window.addEventListener("DOMContentLoaded", fetchData);

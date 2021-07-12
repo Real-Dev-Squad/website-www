c83386826074f18d12c1fbd0e1df13a1be272a3a
Vividh Bhardwaj <61474917+Vividh25@users.noreply.github.com>
2021-07-12 22:23:23 +0530

    Merging changes from the develop branch to the main branch. (#119)
    * Removed references to EmberJS Site from navbars from all files(2 HTML files) (#113)
    
    * Update events.html
    
    I have made the navbar same on both the pages.
    
    * Add query param for signup page
    
    * Updated styling of links
    
    * added CONTRIBUTE.md file
    
    * Updated CONTRIBUTE.md by adding contents of the image as text
    
    * Update CONTRIBUTE.md
    
    * setting up prettierrc.json (#128)
    
    Co-authored-by: anshika-nickelfox <anshika@nickelfox.co>
    
    * Added Check Script (#129)
    
    * Added Check Script
    
    * Added Check Script
    
    * Changed check name from build to Lint-Check
    
    * updated contribute.md
    
    * Member Animation
    
    * Code Enhancement
    
    * Added a shimmer UI Effect. (#134)
    
    * Shimmer UI effect added
    
    * Dead code removed
    
    Co-authored-by: Shivamyana19 <72166316+Shivamyana19@users.noreply.github.com>
    Co-authored-by: Aditi-Singh16 <74586135+Aditi-Singh16@users.noreply.github.com>
    Co-authored-by: Sumit Dhanania <sumitdhanania94@gmail.com>
    Co-authored-by: VaradAC <46750431+VaradAC@users.noreply.github.com>
    Co-authored-by: Anshika Jain <60848927+anshika29@users.noreply.github.com>
    Co-authored-by: anshika-nickelfox <anshika@nickelfox.co>
    Co-authored-by: Himanshu Kr. Mishra <himanshukm2301@gmail.com>
    Co-authored-by: Rahil Kapoor <rahilkapoor2000@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,33 +1,84 @@
-function setUserGreeting(username, firstName) {
+const setUserGreeting = (username, firstName) => {
   if (username) {
-    const userLoginEl = document.querySelector(".btn-login");
+    const userLoginEl = document.querySelector('.btn-login');
 
-    const greetingEl = document.querySelector(".user-greet");
-    const msgGreetMsgEl = document.querySelector(".user-greet-msg");
-    const userImgEl = document.querySelector(".user-profile-pic");
+    const greetingEl = document.querySelector('.user-greet');
+    const msgGreetMsgEl = document.querySelector('.user-greet-msg');
+    const userImgEl = document.querySelector('.user-profile-pic');
 
     const greetMsg = `Hello, ${firstName}!`;
     msgGreetMsgEl.innerText = greetMsg;
     const userImgURL = `https://raw.githubusercontent.com/Real-Dev-Squad/website-static/main/members/${username}/img.png`;
     userImgEl.src = userImgURL;
 
-    greetingEl.style.display = "block";
-    userLoginEl.style.display = "none";
+    greetingEl.style.display = 'block';
+    userLoginEl.style.display = 'none';
   }
-}
+};
 
-function fetchData() {
-  fetch("https://api.realdevsquad.com/users/self", {
-    headers: { "content-type": "application/json" },
-    credentials: "include",
+const selectRandom = (memberImgArr, n) => {
+  const result = new Set();
+  const len = memberImgArr.length;
+  if (n > len)
+    throw new RangeError('selectRandom: more elements taken than available');
+  while (result.size !== n) {
+    const x = Math.floor(Math.random() * len);
+    result.add(memberImgArr[x]);
+  }
+  return result;
+};
+
+const displayMemberImgs = (memberImgArr) => {
+  const images = selectRandom(memberImgArr, 5);
+  const memberImg = document.querySelectorAll('.member-img');
+  let i = 0;
+  for (const img of images) {
+    memberImg[i++].src = img.img_url;
+  }
+  document.getElementById('shimmer').style.display = 'none';
+  document.getElementById('members').style.display = 'flex';
+};
+
+const getImgURL = (rdsId) =>
+  `https://raw.githubusercontent.com/Real-Dev-Squad/website-static/main/members/${rdsId}/img.png`;
+
+const getMemberImgs = () => {
+  const memberImgArray = [];
+  fetch('https://api.realdevsquad.com/members', {
+    cache: 'default',
+    headers: {
+      'content-type': 'application/json',
+    },
+    credentials: 'include',
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      const { members } = res;
+      for (const { isMember, username } of members) {
+        memberImgArray.push({
+          isMember,
+          username,
+          img_url: getImgURL(username),
+        });
+      }
+      const memberImgArr = memberImgArray.filter((person) => person.isMember);
+      displayMemberImgs(memberImgArr);
+    });
+};
+
+const fetchData = () => {
+  fetch('https://api.realdevsquad.com/users/self', {
+    headers: { 'content-type': 'application/json' },
+    credentials: 'include',
   })
     .then((res) => res.json())
     .then((res) => {
       if (res.incompleteUserDetails) {
-        return window.location.replace("https://my.realdevsquad.com/signup");
+        return window.location.replace('https://my.realdevsquad.com/signup');
       }
       setUserGreeting(res.username, res.first_name);
     });
-}
+};
 
-window.addEventListener("DOMContentLoaded", fetchData);
+window.addEventListener('DOMContentLoaded', fetchData);
+window.addEventListener('DOMContentLoaded', getMemberImgs);

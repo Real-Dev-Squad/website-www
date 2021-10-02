932344f7f86ad6fc2bbb14c717d58a6b6cfc756c
Abhinav Mishra <86315672+abhinav1590@users.noreply.github.com>
2021-10-02 16:52:33 +0530

    Art Creator Feature  (#155)
    * Art feature
    
    * made the button non functional
    
    * removed linear gradient from the button

---
+++ js/artFeature.js
@@ -0,0 +1,19 @@
+const sanitizeHtml = (str) => {
+  if (str === null || str === '') return false;
+  str = str.toString();
+  return str.replace(/(<([^>]+)>)/gi, '');
+};
+
+const creatingArtFromHtml = () => {
+  const htmlCode = document.getElementById('html-code').value;
+  const sanitizedHtmlCode = `<div></div> <style> div { ${sanitizeHtml(
+    htmlCode,
+  )} } </style>`;
+  const sanitizeOutputCode = document.getElementById('output');
+  sanitizeOutputCode.setAttribute('html-code', sanitizedHtmlCode);
+  sanitizeOutputCode.contentDocument.body.innerHTML = sanitizedHtmlCode;
+};
+
+document
+  .querySelector('html-code')
+  .addEventListener('keyup', creatingArtFromHtml);

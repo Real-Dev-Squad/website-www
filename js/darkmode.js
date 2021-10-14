d0eeb763adcb88fc2d156451dd7790047e322f9b
Rohan Raj Gupta <78433013+rohan09-raj@users.noreply.github.com>
2021-10-14 13:20:14 +0530

    Formatting fixes

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,18 +1,21 @@
-var icon = document.getElementById("icon");
-icon.onclick =function () {
-    const wasDarkmode = localStorage.getItem('darkmode') === 'true';
-    localStorage.setItem('darkmode', !wasDarkmode);
-    const element = document.body;
-    element.classList.toggle('dark-theme', !wasDarkmode);
-    if (document.body.classList.contains("dark-theme")) {
-        icon.src = "img/sun.png";
-    } else {
-        icon.src = "img/moon.png";
-    }
-}
+var icon = document.getElementById('icon');
+icon.onclick = function () {
+  const wasDarkmode = localStorage.getItem('darkmode') === 'true';
+  localStorage.setItem('darkmode', !wasDarkmode);
+  const element = document.body;
+  element.classList.toggle('dark-theme', !wasDarkmode);
+  if (document.body.classList.contains('dark-theme')) {
+    icon.src = 'img/sun.png';
+  } else {
+    icon.src = 'img/moon.png';
+  }
+};
 
 function onload() {
-    document.body.classList.toggle('dark-theme', localStorage.getItem('darkmode') === 'true');
+  document.body.classList.toggle(
+    'dark-theme',
+    localStorage.getItem('darkmode') === 'true',
+  );
 }
 
 onload = onload();

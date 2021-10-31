6f0dff1a11228febd6e5c32ef3fe83732c8348ab
Rohan Raj Gupta <78433013+rohan09-raj@users.noreply.github.com>
2021-11-01 00:21:51 +0530

    Added a cookie, dark-mode to events-site

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,21 +1,45 @@
-var icon = document.getElementById('icon');
+onload = onload();
+const icon = document.getElementById('icon');
 icon.onclick = function () {
-  const wasDarkmode = localStorage.getItem('darkmode') === 'true';
-  localStorage.setItem('darkmode', !wasDarkmode);
   const element = document.body;
-  element.classList.toggle('dark-theme', !wasDarkmode);
-  if (document.body.classList.contains('dark-theme')) {
+  element.classList.toggle('dark-theme');
+  if (element.classList.contains('dark-theme')) {
     icon.src = 'img/sun.png';
+    setCookie('theme', 'dark', 30);
   } else {
     icon.src = 'img/moon.png';
+    setCookie('theme', 'light', 30);
   }
 };
 
 function onload() {
-  document.body.classList.toggle(
-    'dark-theme',
-    localStorage.getItem('darkmode') === 'true',
-  );
+  const themeIcon = document.getElementById('icon');
+  if (getCookie('theme') === 'light') {
+    document.body.classList.remove('dark-theme');
+    themeIcon.src = 'img/moon.png';
+  } else if (getCookie('theme') === 'dark') {
+    document.body.classList.add('dark-theme');
+    themeIcon.src = 'img/sun.png';
+  }
 }
 
-onload = onload();
+function setCookie(name, value, days) {
+  var expires = '';
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = '; expires=' + date.toUTCString();
+  }
+  document.cookie = name + '=' + (value || '') + expires + '; path=/';
+}
+
+function getCookie(name) {
+  var nameEQ = name + '=';
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}

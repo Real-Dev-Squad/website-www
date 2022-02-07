f8b63547ca8b5b29e440d65187865b67e5772c6a
rushabh-t <trshb28@gmail.com>
2022-02-07 18:39:18 +0530

    Draft PR, yet to make changes discussed

--- js/artFeature.js
+++ js/artFeature.js
@@ -17,3 +17,15 @@ const creatingArtFromHtml = () => {
 document
   .querySelector('#html-code')
   .addEventListener('keyup', creatingArtFromHtml);
+
+const instructionButton = document.getElementById('instruction-button');
+const closeButton = document.getElementsByClassName('close')[0];
+const instructionModal = document.getElementById('instruction-modal');
+
+instructionButton.onclick = () => (instructionModal.style.display = 'block');
+closeButton.onclick = () => (instructionModal.style.display = 'none');
+window.onclick = (event) => {
+  if (event.target === instructionModal) {
+    instructionModal.style.display = 'none';
+  }
+};

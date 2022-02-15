e2271ea520df73a852ad2c5be54fcf0b2bcee025
anmolraidewan <anmoldewanrai@gmail.com>
2022-02-15 14:43:15 +0530

    artwork firebase integrated

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC58EQXfApUffrrVIR-0fyf8lAnGW2F3Hg',
-  authDomain: 'fir-9-dojo-af514.firebaseapp.com',
-  projectId: 'fir-9-dojo-af514',
-  storageBucket: 'fir-9-dojo-af514.appspot.com',
-  messagingSenderId: '94598091750',
-  appId: '1:94598091750:web:e16e496071e0f3f2233c27',
+  apiKey: ART_FEATURE_FIREBASE_API_KEY,
+  authDomain: ART_FEATURE_FIREBASE_AUTH_DOMAIN,
+  projectId: ART_FEATURE_FIREBASE_PROJECT_ID,
+  storageBucket: ART_FEATURE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: ART_FEATURE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: ART_FEATURE_FIREBASE_APP_ID,
 };
 
 // init firebase

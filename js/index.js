a42ebd9e6a5649ba68bcedf825d1814c4047abe8
palak-gupta <palakgupta2712@gmail.com>
2022-03-28 12:09:22 +0530

    card feature added

--- js/index.js
+++ js/index.js
@@ -75,3 +75,33 @@ if (doesGitHubCookieExist()) {
 }
 
 window.addEventListener('DOMContentLoaded', getMemberImgs);
+console.log('Hello');
+const modalTriggers = document.querySelectorAll('.popup-trigger');
+const modalCloseTrigger = document.querySelector('.popup-modal__close');
+const bodyBlackout = document.querySelector('.body-blackout');
+
+modalTriggers.forEach((trigger) => {
+  trigger.addEventListener('click', () => {
+    const { popupTrigger } = trigger.dataset;
+    console.log('click');
+    const popupModal = document.querySelector(
+      `[data-popup-modal="${popupTrigger}"]`,
+    );
+
+    popupModal.classList.add('is--visible');
+    bodyBlackout.classList.add('is-blacked-out');
+
+    popupModal
+      .querySelector('.popup-modal__close')
+      .addEventListener('click', () => {
+        popupModal.classList.remove('is--visible');
+        bodyBlackout.classList.remove('is-blacked-out');
+      });
+
+    bodyBlackout.addEventListener('click', () => {
+      // TODO: Turn into a function to close modal
+      popupModal.classList.remove('is--visible');
+      bodyBlackout.classList.remove('is-blacked-out');
+    });
+  });
+});

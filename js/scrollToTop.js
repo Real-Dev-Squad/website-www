1f72b44a767bfb20ab38571c6dd5300aaa4db402
rohan09-raj <rajrohan1914@gmail.com>
2022-04-16 18:07:14 +0530

    removed extra file

--- js/scrollToTop.js
+++
@@ -1,9 +0,0 @@
-const scrollToTopBtn = document.querySelector('#scrollToTopBtn');
-
-scrollToTopBtn.addEventListener('click', () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: 'smooth',
-  });
-});

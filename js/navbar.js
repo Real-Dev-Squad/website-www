2c37fd9f5ceadb2137ee17f014d97f93fbbd9f71
RitikJaiswal75 <ritik12103032000@gmail.com>
2022-08-20 10:54:14 +0530

    seperated js from html and used css variables

--- js/navbar.js
+++ js/navbar.js
@@ -5,3 +5,24 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
+
+const signout = () => {
+  fetch('https://api.realdevsquad.com/auth/signout', {
+    method: 'GET',
+    credentials: 'include',
+  }).then(() => {
+    location.reload();
+  });
+};
+
+document.getElementById('signout-option').addEventListener('click', () => {
+  signout();
+});
+
+document.querySelectorAll('.user-greet').forEach((greet) => {
+  greet.addEventListener('click', () => {
+    document.querySelectorAll('.dropdown').forEach((dropdown) => {
+      dropdown.classList.toggle('hide');
+    });
+  });
+});

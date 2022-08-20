e2164b5ee0abd5d76842536483de1feb08e7014b
Ankush Dharkar <ankushdharkar@users.noreply.github.com>
2022-08-20 12:02:49 -0700

    Dev to Main (#269)
    * Create LICENSE
    
    * feat(I-258): added posthog analytics to all pages (#259)
    
    * adding if condition in user.js for no profile pic
    
    * changed profile image resolution and prettier issues
    
    * reverted the analytics and removed if else in user.js adding prettier ignore
    
    * Added a dropdown in user greet (#268)
    
    * Added a dropdown in user greet
    
    * removed html onclick attribute and added a event listener
    
    * seperated js from html and used css variables
    
    * fixed variable names
    
    * Fixed formatting of anchor tag
    
    * fixed spacing in dropdown and changed to queryselector
    
    * dropdown item padding shorthand
    
    Co-authored-by: Rohan Singh <75846973+oneonone97@users.noreply.github.com>
    Co-authored-by: Shubham Yadav <46373689+shubham-y@users.noreply.github.com>
    Co-authored-by: RitikJaiswal75 <ritik12103032000@gmail.com>
    Co-authored-by: Rohan Raj Gupta <78433013+rohan09-raj@users.noreply.github.com>
    Co-authored-by: Ritik Jaiswal <57758447+RitikJaiswal75@users.noreply.github.com>

--- js/navbar.js
+++ js/navbar.js
@@ -5,3 +5,22 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
+
+const signout = () => {
+  fetch('https://api.realdevsquad.com/auth/signout', {
+    method: 'GET',
+    credentials: 'include',
+  }).then(() => {
+    location.reload();
+  });
+};
+
+document.getElementById('signout-option').addEventListener('click', () => {
+  signout();
+});
+
+document.querySelectorAll('.user-greet').forEach((greet) => {
+  greet.addEventListener('click', () => {
+    document.querySelector('.dropdown').classList.toggle('hide');
+  });
+});

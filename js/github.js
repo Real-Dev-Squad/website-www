94fbeaca047f96112f930e2228f80199a4bea6ae
Vinayak <vinayak20029@gmail.com>
2022-08-28 02:00:49 +0530

    Lint Fixed

--- js/github.js
+++ js/github.js
@@ -1,5 +1,5 @@
 // VF Flow: https://github.com/Real-Dev-Squad/website-www/issues/233
-const GITHUB_MOCK_URL = "https://github.realdevsquad.com/"
+const GITHUB_MOCK_URL = 'https://github.realdevsquad.com/';
 
 const doesGitHubCookieExist = () => {
   const cookieStr = document.cookie || '';
@@ -11,38 +11,38 @@ const doesGitHubCookieExist = () => {
 };
 
 const SIGN_IN_GITHUB_OAUTH = () => {
-    let originURL = window?.location?.origin ?? "/";
-    let githubOauth = 'https://github.com/login/oauth/authorize?client_id=45f5cee93a0396939229&state=';
-    let encodedOriginURL = btoa(originURL)
+  let originURL = window?.location?.origin ?? '/';
+  let githubOauth =
+    'https://github.com/login/oauth/authorize?client_id=45f5cee93a0396939229&state=';
+  let encodedOriginURL = btoa(originURL);
 
-    return githubOauth + encodedOriginURL;
-}
+  return githubOauth + encodedOriginURL;
+};
 
 const updateGitHubLink = () => {
-    const githubMock = JSON.parse(localStorage.getItem('githubMock')) ?? {};
-    const githubMockExpired = new Date().getTime() > (githubMock.expiresIn ?? 0);
+  const githubMock = JSON.parse(localStorage.getItem('githubMock')) ?? {};
+  const githubMockExpired = new Date().getTime() > (githubMock.expiresIn ?? 0);
 
-    let signUpLink = GITHUB_MOCK_URL
+  let signUpLink = GITHUB_MOCK_URL;
 
-    if (githubMock.visited && !githubMockExpired) {
-        signUpLink = SIGN_IN_GITHUB_OAUTH()
-    }
+  if (githubMock.visited && !githubMockExpired) {
+    signUpLink = SIGN_IN_GITHUB_OAUTH();
+  }
 
-    const allLoginBtns = document.querySelectorAll('.btn-login');
-    allLoginBtns.forEach((btn) => {
-        btn.setAttribute('href', signUpLink);
+  const allLoginBtns = document.querySelectorAll('.btn-login');
+  allLoginBtns.forEach((btn) => {
+    btn.setAttribute('href', signUpLink);
 
-
-        btn.addEventListener('click', (event) => {
-            event.preventDefault()
-            let githubMock = {
-                visited: true,
-                expiresIn: new Date().getTime() + 15 ** 9
-            }
-            localStorage.setItem('githubMock', JSON.stringify(githubMock))
-            window.location = btn.getAttribute('href')
-        })
+    btn.addEventListener('click', (event) => {
+      event.preventDefault();
+      let githubMock = {
+        visited: true,
+        expiresIn: new Date().getTime() + 15 ** 9,
+      };
+      localStorage.setItem('githubMock', JSON.stringify(githubMock));
+      window.location = btn.getAttribute('href');
     });
+  });
 };
 
 export { doesGitHubCookieExist, updateGitHubLink };

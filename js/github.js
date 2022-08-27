0e7816569562e9006bea51b37b31a70252b4d53b
Vinayak <vinayak20029@gmail.com>
2022-08-28 01:45:55 +0530

    "new signupflow added"

--- js/github.js
+++ js/github.js
@@ -1,4 +1,5 @@
 // VF Flow: https://github.com/Real-Dev-Squad/website-www/issues/233
+const GITHUB_MOCK_URL = "https://github.realdevsquad.com/"
 
 const doesGitHubCookieExist = () => {
   const cookieStr = document.cookie || '';
@@ -9,11 +10,39 @@ const doesGitHubCookieExist = () => {
   return !!githubCookieStr;
 };
 
+const SIGN_IN_GITHUB_OAUTH = () => {
+    let originURL = window?.location?.origin ?? "/";
+    let githubOauth = 'https://github.com/login/oauth/authorize?client_id=45f5cee93a0396939229&state=';
+    let encodedOriginURL = btoa(originURL)
+
+    return githubOauth + encodedOriginURL;
+}
+
 const updateGitHubLink = () => {
-  const allLoginBtns = document.querySelectorAll('.btn-login');
-  allLoginBtns.forEach((btn) => {
-    btn.setAttribute('href', 'https://github.realdevsquad.com');
-  });
+    const githubMock = JSON.parse(localStorage.getItem('githubMock')) ?? {};
+    const githubMockExpired = new Date().getTime() > (githubMock.expiresIn ?? 0);
+
+    let signUpLink = GITHUB_MOCK_URL
+
+    if (githubMock.visited && !githubMockExpired) {
+        signUpLink = SIGN_IN_GITHUB_OAUTH()
+    }
+
+    const allLoginBtns = document.querySelectorAll('.btn-login');
+    allLoginBtns.forEach((btn) => {
+        btn.setAttribute('href', signUpLink);
+
+
+        btn.addEventListener('click', (event) => {
+            event.preventDefault()
+            let githubMock = {
+                visited: true,
+                expiresIn: new Date().getTime() + 15 ** 9
+            }
+            localStorage.setItem('githubMock', JSON.stringify(githubMock))
+            window.location = btn.getAttribute('href')
+        })
+    });
 };
 
 export { doesGitHubCookieExist, updateGitHubLink };

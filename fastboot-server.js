29156a736585c82c72135b9247386b08d119086d
rohan09-raj <rajrohan1914@gmail.com>
2022-09-18 17:14:24 +0530

    Initial Commit

---
+++ fastboot-server.js
@@ -0,0 +1,16 @@
+const FastBootAppServer = require('fastboot-app-server');
+
+const MY_GLOBAL = 'MY GLOBAL';
+
+let server = new FastBootAppServer({
+  distPath: 'dist/',
+  gzip: true, // Optional - Enables gzip compression.
+  buildSandboxGlobals(defaultGlobals) {
+    // Optional - Make values available to the Ember app running in the FastBoot server, e.g. "MY_GLOBAL" will be available as "GLOBAL_VALUE"
+    return Object.assign({}, defaultGlobals, { GLOBAL_VALUE: MY_GLOBAL });
+  },
+  log: true, // Optional - Specifies whether the server should use its default request logging. Useful for turning off default logging when providing custom logging middlewares
+  chunkedResponse: true, // Optional - Opt-in to chunked transfer encoding, transferring the head, body and potential shoeboxes in separate chunks. Chunked transfer encoding should have a positive effect in particular when the app transfers a lot of data in the shoebox.
+});
+
+server.start();
--- js/featureFlag.js
+++
@@ -1,5 +0,0 @@
-const joinSection = document.getElementById('section-join');
-const params = new URLSearchParams(window.location.search);
-if (params.get('join') === 'true') {
-  joinSection.classList.remove('hide');
-}

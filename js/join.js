56dee3166ba35a1e3dea478d8d33fdaa5d865f38
RitikJaiswal75 <ritik12103032000@gmail.com>
2022-09-22 20:25:57 +0530

    Added a datavalidator for checking field values and support of autofill

--- js/join.js
+++ js/join.js
@@ -14,6 +14,18 @@ const skills = document.getElementById('skills');
 const college = document.getElementById('college');
 const forFun = document.getElementById('for-fun');
 const funFact = document.getElementById('fun-fact');
+const next2 = document.getElementById('next2');
+const previous2 = document.getElementById('previous2');
+
+function dataValidator(element, size) {
+  if (element.value.trim().length > size) {
+    element.classList.add('correct-data');
+    element.classList.remove('incorrect-data');
+  } else {
+    element.classList.remove('correct-data');
+    element.classList.add('incorrect-data');
+  }
+}
 
 function toggleButton(page = 'page2') {
   if (page === 'page2') {
@@ -26,6 +38,20 @@ function toggleButton(page = 'page2') {
     } else {
       if (!next1.classList.contains('button-disabled')) {
         next1.classList.add('button-disabled');
+      }
+    }
+  } else if (page === 'page3') {
+    if (
+      introduction.value.trim().length > 100 &&
+      skills.value.trim().length > 50 &&
+      college.value.trim().length > 25 &&
+      forFun.value.trim().length > 100 &&
+      funFact.value.trim().length > 100
+    ) {
+      next2.classList.remove('button-disabled');
+    } else {
+      if (!next2.classList.contains('button-disabled')) {
+        next2.classList.add('button-disabled');
       }
     }
   }
@@ -44,6 +70,16 @@ function fieldAutofill() {
   city.value = window.localStorage.getItem('city');
   state.value = window.localStorage.getItem('state');
   country.value = window.localStorage.getItem('country');
+  introduction.value = window.localStorage.getItem('introduction');
+  skills.value = window.localStorage.getItem('skills');
+  college.value = window.localStorage.getItem('college');
+  forFun.value = window.localStorage.getItem('forFun');
+  funFact.value = window.localStorage.getItem('funFact');
+  dataValidator(introduction, 100);
+  dataValidator(skills, 50);
+  dataValidator(college, 25);
+  dataValidator(forFun, 100);
+  dataValidator(funFact, 100);
 }
 
 //Direct to the page user left from
@@ -65,6 +101,12 @@ window.addEventListener('load', () => {
     page3.classList.remove('hide-Page');
     fieldAutofill();
     toggleButton();
+  } else if (currentFlowState == 3) {
+    page1.classList.add('hide-page');
+    page2.classList.add('hide-page');
+    page3.classList.remove('hide-Page');
+    fieldAutofill();
+    toggleButton('page3');
   }
 });
 
@@ -87,6 +129,36 @@ state.addEventListener('input', () => {
 country.addEventListener('input', () => {
   window.localStorage.setItem('country', country.value);
   toggleButton();
+});
+
+introduction.addEventListener('input', () => {
+  window.localStorage.setItem('introduction', introduction.value);
+  toggleButton('page3');
+  dataValidator(introduction, 100);
+});
+
+skills.addEventListener('input', () => {
+  window.localStorage.setItem('skills', skills.value);
+  toggleButton('page3');
+  dataValidator(skills, 50);
+});
+
+college.addEventListener('input', () => {
+  window.localStorage.setItem('college', college.value);
+  toggleButton('page3');
+  dataValidator(college, 25);
+});
+
+forFun.addEventListener('input', () => {
+  window.localStorage.setItem('forFun', forFun.value);
+  toggleButton('page3');
+  dataValidator(forFun, 100);
+});
+
+funFact.addEventListener('input', () => {
+  window.localStorage.setItem('funFact', funFact.value);
+  toggleButton('page3');
+  dataValidator(funFact, 100);
 });
 
 //Button Enablers

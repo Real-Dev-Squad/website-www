48530d35b7019ea2573ff7b616bcec04a3f55188
PRANKUR PANDEY <pprankur@gmail.com>
2022-11-03 09:01:05 +0530

    Fixed : now can login with github directly

--- js/github.js
+++ js/github.js
@@ -8,13 +8,11 @@ const doesGitHubCookieExist = () => {
 
   return !!githubCookieStr;
 };
-// const oldRedirectURL= `https://github.realdevsquad.com`
-const redirectURL= `https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97`
 
 const updateGitHubLink = () => {
   const allLoginBtns = document.querySelectorAll('.btn-login');
   allLoginBtns.forEach((btn) => {
-    btn.setAttribute('href', redirectURL);
+    btn.setAttribute('href', 'https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97');
   });
 };
 

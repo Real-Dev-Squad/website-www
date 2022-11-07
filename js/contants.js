030b15017c52fc4a234c677c353c30e39724dd41
ivinayakg <vinayak20029@gmail.com>
2022-11-07 23:51:00 +0530

    updated the flow and removed the mock github

--- js/contants.js
+++ js/contants.js
@@ -1,4 +1,3 @@
 export const GITHUB_MOCK_URL = 'https://github.realdevsquad.com/';
 export const GITHUB_OAUTH =
   'https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97';
-export const GITHUB_OBJECT_KEY = 'githubMock';
--- js/github.js
+++ js/github.js
@@ -1,7 +1,6 @@
 // VF Flow: https://github.com/Real-Dev-Squad/website-www/issues/233
 import {
   GITHUB_MOCK_URL,
-  GITHUB_OBJECT_KEY,
   GITHUB_OAUTH,
 } from './contants.js';
 
@@ -15,35 +14,18 @@ const doesGitHubCookieExist = () => {
 };
 
 const signInGitHubOAuth = () => {
-  const originURL = window.location.origin;
+  const originURL = window.location.href;
   if (!originURL) return GITHUB_OAUTH;
   return GITHUB_OAUTH + '&state=' + originURL;
 };
 
 const updateGitHubLink = () => {
-  const githubMock = JSON.parse(localStorage.getItem(GITHUB_OBJECT_KEY)) ?? {};
-  const isGithubMockExpired =
-    new Date().getTime() > (githubMock.expiresIn ?? 0);
-
-  let signUpLink = GITHUB_MOCK_URL;
-  if (githubMock.visited && !isGithubMockExpired) {
-    signUpLink = signInGitHubOAuth();
-  }
+  let signUpLink = signInGitHubOAuth();
 
   const allLoginBtns = document.querySelectorAll('.btn-login');
 
   allLoginBtns.forEach((btn) => {
     btn.setAttribute('href', signUpLink);
-
-    btn.addEventListener('click', (event) => {
-      event.preventDefault();
-      const githubMock = {
-        visited: true,
-        expiresIn: new Date().getTime() + 15 ** 9,
-      };
-      localStorage.setItem(GITHUB_OBJECT_KEY, JSON.stringify(githubMock));
-      window.location = btn.getAttribute('href');
-    });
   });
 };
 

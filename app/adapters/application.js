87912a2c09af6ed195c20a5074092d4934073c0d
rohan09-raj <rajrohan1914@gmail.com>
2022-11-29 07:45:12 +0530

    Created a navbar

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,7 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
+
 import ENV from 'website-www/config/environment';
+
 export default class ApplicationAdapter extends JSONAPIAdapter {
   host = ENV.BASE_API_URL;
 

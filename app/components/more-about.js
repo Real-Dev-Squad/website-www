14574d3bd8fe70144b3d67fdfcf469d143e95788
Rohan Raj Gupta <78433013+rohan09-raj@users.noreply.github.com>
2022-12-07 22:29:40 +0530

    [Ember-Migration] Created a navbar (#325)
    * Created a navbar
    
    * minor refactoring and fixes
    
    * resolved over simplications and minor fixes

--- app/components/more-about.js
+++ app/components/more-about.js
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
-import { APPS } from '../constants/urls';
+import { APPS, ABOUT } from '../constants/urls';
 
 export default class MoreAboutComponent extends Component {
   MEMBERS_URL = APPS.MEMBERS;
-  FAQ_URL = APPS.FAQ;
+  FAQ_URL = ABOUT.FAQ;
 }

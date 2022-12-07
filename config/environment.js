97196c1642f383a9ca011f57f17da4d13586d125
rohan09-raj <rajrohan1914@gmail.com>
2022-12-08 01:11:33 +0530

    created footer

--- config/environment.js
+++ config/environment.js
@@ -19,7 +19,7 @@ module.exports = function (environment) {
     },
   };
 
-  ENV.BASE_API_URL = 'https://api.realdevsquad.com';
+  ENV.BASE_API_URL = 'http://localhost:3000';
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;

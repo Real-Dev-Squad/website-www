faba8261742b40ff431985347444f9beec486a14
Rohan Raj Gupta <78433013+rohan09-raj@users.noreply.github.com>
2022-12-08 11:06:49 +0530

    [Ember-Migration] Created a header component (#329)
    * Created a navbar
    
    * minor refactoring and fixes
    
    * Created header component
    
    * resolved over simplications and minor fixes
    
    * Created a navbar
    
    * minor refactoring and fixes
    
    * Created a navbar
    
    * minor fixes
    
    * Removed repetition of code

--- app/constants/urls.js
+++ app/constants/urls.js
@@ -12,3 +12,26 @@ export const AUTH = {
   SIGN_IN:
     'https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97',
 };
+
+export const SOCIALS = {
+  LINKEDIN: {
+    URL: 'https://www.linkedin.com/company/real-dev-squad',
+    TITLE: 'LinkedIn',
+    ICON: 'linkedin',
+  },
+  TWITTER: {
+    URL: 'https://twitter.com/realdevsquad?lang=en',
+    TITLE: 'Twitter',
+    ICON: 'twitter',
+  },
+  FACEBOOK: {
+    URL: 'https://www.facebook.com/Real-Dev-Squad-108713777585062/',
+    TITLE: 'Facebook',
+    ICON: 'facebook',
+  },
+  INSTAGRAM: {
+    URL: 'https://www.instagram.com/realdevsquad/?hl=en',
+    TITLE: 'Instagram',
+    ICON: 'instagram',
+  },
+};

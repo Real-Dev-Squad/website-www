4a94ba29d21c04fd851e8d2045c0b5a00629707a
rohan09-raj <rajrohan1914@gmail.com>
2022-12-11 19:07:44 +0530

    added description component tests

--- tests/integration/components/description-test.js
+++ tests/integration/components/description-test.js
@@ -6,21 +6,17 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | description', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+  test('description content renders', async function (assert) {
     await render(hbs`<Description />`);
 
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <Description>
-        template block text
-      </Description>
-    `);
-
-    assert.dom(this.element).hasText('template block text');
+    assert.dom('[data-test-description-img]').exists();
+    assert
+      .dom('[data-test-description-title]')
+      .hasText('What is Real Dev Squad');
+    assert
+      .dom('[data-test-description-content]')
+      .hasText(
+        'Real Dev Squad is an online non-profit open source free fun community for people in tech, mainly developers, designers, college students, or product managers, to come, learn and contribute towards building a platform for our community, that helps upskill everyone. We are an inclusive, respectful, warm, motivated and commited squad of people who constantly grow together and tackle bigger and harder challenges to ensure we become some of the best problem solvers, engineers, designers and more out there.'
+      );
   });
 });

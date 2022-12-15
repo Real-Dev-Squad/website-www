9fe606976454c06e491c5303475a6e6972253529
rohan09-raj <rajrohan1914@gmail.com>
2022-12-15 20:57:06 +0530

    added tests for cards

--- tests/integration/components/cards-test.js
+++ tests/integration/components/cards-test.js
@@ -1,26 +1,35 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'website-www/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | cards', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+  test('cards renders', async function (assert) {
     await render(hbs`<Cards />`);
 
-    assert.dom(this.element).hasText('');
+    assert.dom('[data-test-title]').hasText('Why you should consider joining?');
+    assert.dom('[data-test-modal-backdrop]');
+    assert.dom('[data-test-card]').exists({ count: 6 });
+    assert.dom('[data-test-card-title]').exists({ count: 6 });
+    assert.dom('[data-test-card-img]').exists({ count: 6 });
+    assert.dom('[data-test-card-content]').exists({ count: 6 });
+    assert.dom('[data-test-card-btn]').exists({ count: 6 });
+  });
 
-    // Template block usage:
-    await render(hbs`
-      <Cards>
-        template block text
-      </Cards>
-    `);
+  test('modal toggling on clicking card', async function (assert) {
+    await render(hbs`<Cards />`);
 
-    assert.dom(this.element).hasText('template block text');
+    assert.dom('[data-test-card-modal]').doesNotExist();
+    await click('[data-test-card]');
+    assert.dom('[data-test-card-modal]').exists();
+    assert.dom('[data-test-modal-backdrop]').exists();
+    assert.dom('[data-test-card-modal-close]').exists();
+    assert.dom('[data-test-card-modal-title]').exists();
+    assert.dom('[data-test-card-modal-img]').exists();
+    assert.dom('[data-test-card-modal-content]').exists();
+    await click('[data-test-card-modal-close]');
+    assert.dom('[data-test-card-modal]').doesNotExist();
   });
 });

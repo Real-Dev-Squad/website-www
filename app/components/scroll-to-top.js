92988aa93dd1e3719f0a18d9cd1005e36d6d0a06
rohan09-raj <rajrohan1914@gmail.com>
2022-12-15 22:12:38 +0530

    removed redundant code

--- app/components/scroll-to-top.js
+++ app/components/scroll-to-top.js
@@ -7,17 +7,4 @@ export default class ScrollToTopComponent extends Component {
   @action scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
-
-  @action scrollPosition() {
-    window.onscroll = () => {
-      if (
-        document.body.scrollTop > 50 ||
-        document.documentElement.scrollTop > 50
-      ) {
-        this.scrollBtn.style.bottom = '20px';
-      } else {
-        this.scrollBtn.style.bottom = '-100px';
-      }
-    };
-  }
 }

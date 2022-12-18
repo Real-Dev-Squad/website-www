a46b710cb96a5d5961290439f54cd212585049bb
rohan09-raj <rajrohan1914@gmail.com>
2022-12-18 15:43:41 +0530

    resolved failing tests

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,77 +1,11 @@
 import Component from '@glimmer/component';
-import { APPS, ABOUT, SOCIALS } from '../constants/urls';
+import { ABOUT } from '../constants/urls';
+import { APPS_PROPERTIES, ABOUT_PROPERTIES } from '../constants/footer-data';
+import { SOCIAL_LINK_PROPERTIES } from '../constants/social-data';
 
 export default class FooterComponent extends Component {
-  APPS_PROPERTIES = [
-    {
-      url: APPS.HOME,
-      name: 'Home',
-    },
-    {
-      url: APPS.WELCOME,
-      name: 'Welcome',
-    },
-    {
-      url: APPS.EVENTS,
-      name: 'Events',
-    },
-    {
-      url: APPS.MEMBERS,
-      name: 'Members',
-    },
-    {
-      url: APPS.CRYPTO,
-      name: 'Crypto',
-    },
-    {
-      url: APPS.STATUS,
-      name: 'Status',
-    },
-    {
-      url: APPS.PROFILE,
-      name: 'My Profile',
-    },
-  ];
-
-  ABOUT_PROPERTIES = [
-    {
-      url: ABOUT.JOIN,
-      name: 'How to Join?',
-    },
-    {
-      url: ABOUT.FAQ,
-      name: 'FAQ',
-    },
-    {
-      url: ABOUT.DISCORD,
-      name: 'Discord',
-    },
-    {
-      url: ABOUT.COC,
-      name: 'Code of Conduct',
-    },
-  ];
-
-  SOCIAL_LINK_PROPERTIES = [
-    {
-      url: SOCIALS.LINKEDIN.URL,
-      icon: SOCIALS.LINKEDIN.ICON,
-      alt: SOCIALS.LINKEDIN.TITLE,
-    },
-    {
-      url: SOCIALS.TWITTER.URL,
-      icon: SOCIALS.TWITTER.ICON,
-      alt: SOCIALS.TWITTER.TITLE,
-    },
-    {
-      url: SOCIALS.FACEBOOK.URL,
-      icon: SOCIALS.FACEBOOK.ICON,
-      alt: SOCIALS.FACEBOOK.TITLE,
-    },
-    {
-      url: SOCIALS.INSTAGRAM.URL,
-      icon: SOCIALS.INSTAGRAM.ICON,
-      alt: SOCIALS.INSTAGRAM.TITLE,
-    },
-  ];
+  REPOSITORY_URL = ABOUT.REPOSITORY;
+  APPS_PROPERTIES = APPS_PROPERTIES;
+  ABOUT_PROPERTIES = ABOUT_PROPERTIES;
+  SOCIAL_LINK_PROPERTIES = SOCIAL_LINK_PROPERTIES;
 }

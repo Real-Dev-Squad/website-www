36b4c899055759239aeac82dbcdd7713d465e594
rohan09-raj <rajrohan1914@gmail.com>
2022-12-26 23:00:17 +0530

    added some more tests

--- tests/integration/components/navbar-test.js
+++ tests/integration/components/navbar-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'website-www/tests/helpers';
 import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { APPS, AUTH } from '../../constants/urls';
 
 module('Integration | Component | navbar', function (hooks) {
   setupRenderingTest(hooks);
@@ -15,37 +16,24 @@ module('Integration | Component | navbar', function (hooks) {
       <Navbar @isLoggedIn={{this.isLoggedIn}}/>
     `);
 
+    assert.dom('[data-test-home-link]').exists();
+    assert.dom('[data-test-home-img]').exists();
     assert.dom('[data-test-home]').hasText('Home');
-    assert
-      .dom('[data-test-home]')
-      .hasAttribute('href', 'https://realdevsquad.com/');
+    assert.dom('[data-test-home]').hasAttribute('href', APPS.HOME);
     assert.dom('[data-test-welcome]').hasText('Welcome');
-    assert
-      .dom('[data-test-welcome]')
-      .hasAttribute('href', 'https://welcome.realdevsquad.com/');
+    assert.dom('[data-test-welcome]').hasAttribute('href', APPS.WELCOME);
     assert.dom('[data-test-events]').hasText('Events');
-    assert
-      .dom('[data-test-events]')
-      .hasAttribute('href', 'https://realdevsquad.com/events.html/');
+    assert.dom('[data-test-events]').hasAttribute('href', APPS.EVENTS);
     assert.dom('[data-test-members]').hasText('Members');
-    assert
-      .dom('[data-test-members]')
-      .hasAttribute('href', 'https://members.realdevsquad.com/');
+    assert.dom('[data-test-members]').hasAttribute('href', APPS.MEMBERS);
     assert.dom('[data-test-crypto]').hasText('Crypto');
-    assert
-      .dom('[data-test-crypto]')
-      .hasAttribute('href', 'https://crypto.realdevsquad.com/');
+    assert.dom('[data-test-crypto]').hasAttribute('href', APPS.CRYPTO);
     assert.dom('[data-test-status]').hasText('Status');
-    assert
-      .dom('[data-test-status]')
-      .hasAttribute('href', 'https://status.realdevsquad.com/');
+    assert.dom('[data-test-status]').hasAttribute('href', APPS.STATUS);
 
-    assert.dom('[data-test-login-btn]').exists();
-    assert.dom('[data-test-login-btn]').hasText('Sign In with GitHub');
-    //TODO: Fix this asset path
-    assert
-      .dom('[data-test-login-btn-img]')
-      .hasProperty('src', 'http://localhost:4200/assets/icons/github-logo.png');
+    assert.dom('[data-test-login]').hasText('Sign In with GitHub');
+    assert.dom('[data-test-login]').hasAttribute('href', AUTH.SIGN_IN);
+    assert.dom('[data-test-login-img]').exists();
   });
 
   test('navbar renders when user logged in', async function (assert) {
@@ -79,10 +67,11 @@ module('Integration | Component | navbar', function (hooks) {
     assert.dom('[data-test-toggle-button]').exists();
     assert.dom('[data-test-nav-menu]').exists();
 
-    await click('[data-test-toggle-button]');
     assert.dom('[data-test-nav-menu]').doesNotHaveClass('active');
     await click('[data-test-toggle-button]');
-    assert.dom('[data-test-nav-menu]').hasClass('nav__menu');
+    assert.dom('[data-test-nav-menu]').hasClass('active');
+    await click('[data-test-toggle-button]');
+    assert.dom('[data-test-nav-menu]').doesNotHaveClass('active');
   });
 
   test('toggle dropdown menu', async function (assert) {
@@ -105,11 +94,8 @@ module('Integration | Component | navbar', function (hooks) {
     await click('[data-test-dropdown-toggle]');
     assert.dom('[data-test-dropdown]').hasClass('menu');
     assert.dom('[data-test-profile]').hasText('My Profile');
-    assert
-      .dom('[data-test-profile]')
-      .hasAttribute('href', 'https://my.realdevsquad.com/');
-    assert.dom('[data-test-signout]').exists();
-    assert.dom('[data-test-]').hasText('Sign Out');
+    assert.dom('[data-test-profile]').hasAttribute('href', APPS.PROFILE);
+    assert.dom('[data-test-signout]').hasText('Sign Out');
 
     await click('[data-test-dropdown-toggle]');
     assert.dom('[data-test-dropdown]').doesNotHaveClass('active-menu');

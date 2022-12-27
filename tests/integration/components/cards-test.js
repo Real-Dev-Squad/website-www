47b6c499e0d39ddfc2aa4d63c9bff53fe10274d5
rohan09-raj <rajrohan1914@gmail.com>
2022-12-27 15:03:51 +0530

    resolved errors

--- tests/integration/components/cards-test.js
+++ tests/integration/components/cards-test.js
@@ -1,7 +1,5 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'website-www/tests/helpers';
-import { render } from '@ember/test-helpers';
-import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | cards', function (hooks) {
   setupRenderingTest(hooks);

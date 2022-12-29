aeaa02059b3a2ad486349fff4080a342ee03d22a
rohan09-raj <rajrohan1914@gmail.com>
2022-12-29 13:09:20 +0530

    updated the tests

--- tests/integration/components/header-test.js
+++ tests/integration/components/header-test.js
@@ -1,10 +1,14 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'website-www/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | header', function (hooks) {
   setupRenderingTest(hooks);
 
   test('header content renders', async function (assert) {
+    assert.expect(8);
+
     await render(hbs`<Header />`);
 
     assert.dom('[data-test-logo]').exists();
@@ -12,6 +16,8 @@ module('Integration | Component | header', function (hooks) {
     assert.dom('[data-test-title]').hasText('Real Dev Squad');
     assert.dom('[data-test-social-link]').exists({ count: 4 });
     assert.dom('[data-test-social-icon]').exists({ count: 4 });
+    assert.dom('[data-test-vertical-separators]').exists({ count: 3 });
+    assert.dom('[data-test-round-separators]').exists({ count: 3 });
     assert.dom('[data-test-welcome-img]').exists();
   });
 });

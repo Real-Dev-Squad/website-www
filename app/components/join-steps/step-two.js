a07b71a2b169fdd6a3f04d8cf723b3e5b3bcc809
rohan09-raj <rajrohan1914@gmail.com>
2023-01-15 17:50:04 +0530

    minor refactoring

--- app/components/join-steps/step-two.js
+++ app/components/join-steps/step-two.js
@@ -1,3 +1,10 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
-export default class StepTwoComponent extends Component {}
+export default class StepTwoComponent extends Component {
+  @tracked introduction = '';
+  @tracked skills = '';
+  @tracked organization = '';
+  @tracked funActivities = '';
+  @tracked funFacts = '';
+}

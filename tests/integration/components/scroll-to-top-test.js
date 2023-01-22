5311784a7437dc736bba5e04095ff814b6cdb533
Ranita Saha <saharanitaa1230@gmail.com>
2023-01-22 12:03:29 +0530

    feat: added tests for scroll-to-top component

--- tests/integration/components/scroll-to-top-test.js
+++ tests/integration/components/scroll-to-top-test.js
@@ -1,10 +1,17 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'website-www/tests/helpers';
+import { render, click } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | scroll-to-top', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    assert.expect(0);
+    assert.expect(1);
+
+    await render(hbs`<ScrollToTop />`);
+    
+    await click('[data-test-scroll-to-top]');
+    assert.dom('[data-test-scroll-icon]').exists();
   });
 });

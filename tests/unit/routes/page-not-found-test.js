fb5d35b33c130c9a2ea56fcf3692bb44a5b0e99c
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-01-26 14:55:58 +0530

    write test for page-not-found

--- tests/unit/routes/page-not-found-test.js
+++ tests/unit/routes/page-not-found-test.js
@@ -1,11 +1,20 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'website-www/tests/helpers';
-
+import { visit} from '@ember/test-helpers';
 module('Unit | Route | page-not-found', function (hooks) {
   setupTest(hooks);
 
   test('it exists', function (assert) {
     let route = this.owner.lookup('route:page-not-found');
     assert.ok(route);
+  });
+  test('visiting /*', async function (assert) {
+    await visit('/*');
+
+    assert
+      .dom('.page-not-found .page-not-found-img') 
+      .hasAttribute('src', '../assets/images/not-found.png')
+      .hasAttribute('alt', 'not-found');
+    assert.dom('.page-not-found .page-not-found-text').hasText("The page you're looking for cannot be found!");
   });
 });

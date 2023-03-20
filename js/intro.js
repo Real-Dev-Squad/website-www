cc6e0fee9067a2dbc945000a8a942d03231f2a8c
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-03-20 11:30:49 +0530

    fix lint issue

--- js/intro.js
+++ js/intro.js
@@ -1,4 +1,4 @@
-import { BASE_URL, SELF_URL} from './constants.js';
+import { BASE_URL, SELF_URL } from './constants.js';
 
 //fetch API DATA template
 async function makeApiCall(
@@ -24,40 +24,37 @@ async function makeApiCall(
   }
 }
 
-
 //fetching userId
-async function fetchSavedDetails(page){
+async function fetchSavedDetails(page) {
   try {
-    const usersRequest = await makeApiCall(
-      `${SELF_URL}`,
-    );
+    const usersRequest = await makeApiCall(`${SELF_URL}`);
     let usersDataList;
     usersDataList = await usersRequest.json();
     return usersDataList.id;
-  }catch (err) {
+  } catch (err) {
     throw err;
   }
 }
 
 //generate form and render information
-async function generateSavedDetailsForm(users){
-  console.log(users)
-  let renderPage = document.createElement("section");
+async function generateSavedDetailsForm(users) {
+  console.log(users);
+  let renderPage = document.createElement('section');
   renderPage.className = 'render-page';
   renderPage.setAttribute('id', 'render-page');
 
-  let greeting = document.createElement("h1");
+  let greeting = document.createElement('h1');
   greeting.className = 'greeting';
-  greeting.innerText = "Thanks for filling out 👀 Here's what was received."
+  greeting.innerText = "Thanks for filling out 👀 Here's what was received.";
   renderPage.appendChild(greeting);
 
-  let container = document.createElement("div");
+  let container = document.createElement('div');
   container.className = 'container';
   renderPage.appendChild(container);
 
   let firstName = document.createElement('p');
-  firstName.className = "input-label-dark";
-  firstName.innerText = "First Name";
+  firstName.className = 'input-label-dark';
+  firstName.innerText = 'First Name';
   let renderFirstName = document.createElement('p');
   renderFirstName.className = 'user-input input-regular';
   renderFirstName.setAttribute('id', 'renderFName');
@@ -66,7 +63,7 @@ async function generateSavedDetailsForm(users){
   container.appendChild(renderFirstName);
 
   let lastName = document.createElement('p');
-  lastName.className = "input-label-dark";
+  lastName.className = 'input-label-dark';
   lastName.innerText = 'Last Name';
   let renderLastName = document.createElement('p');
   renderLastName.className = 'user-input input-regular';
@@ -76,7 +73,7 @@ async function generateSavedDetailsForm(users){
   container.appendChild(renderLastName);
 
   let yourCity = document.createElement('p');
-  yourCity.className = "input-label-dark";
+  yourCity.className = 'input-label-dark';
   yourCity.innerText = 'Your City';
   let renderCity = document.createElement('p');
   renderCity.className = 'user-input input-regular';
@@ -86,7 +83,7 @@ async function generateSavedDetailsForm(users){
   container.appendChild(renderCity);
 
   let yourState = document.createElement('p');
-  yourState.className = "input-label-dark";
+  yourState.className = 'input-label-dark';
   yourState.innerText = 'Your State';
   let renderState = document.createElement('p');
   renderState.className = 'user-input input-regular';
@@ -96,7 +93,7 @@ async function generateSavedDetailsForm(users){
   container.appendChild(renderState);
 
   let yourCountry = document.createElement('p');
-  yourCountry.className = "input-label-dark";
+  yourCountry.className = 'input-label-dark';
   yourCountry.innerText = 'Your Country';
   let renderCountry = document.createElement('p');
   renderCountry.className = 'user-input input-regular';
@@ -106,7 +103,7 @@ async function generateSavedDetailsForm(users){
   container.appendChild(renderCountry);
 
   let yourIntroduction = document.createElement('p');
-  yourIntroduction.className = "input-label-dark";
+  yourIntroduction.className = 'input-label-dark';
   yourIntroduction.innerText = 'Your Introduction';
   let renderIntro = document.createElement('p');
   renderIntro.className = 'user-input input-big';
@@ -116,7 +113,7 @@ async function generateSavedDetailsForm(users){
   container.appendChild(renderIntro);
 
   let yourSkills = document.createElement('p');
-  yourSkills.className = "input-label-dark";
+  yourSkills.className = 'input-label-dark';
   yourSkills.innerText = 'Your Skills';
   let renderSkills = document.createElement('p');
   renderSkills.className = 'user-input input-regular';
@@ -126,7 +123,7 @@ async function generateSavedDetailsForm(users){
   container.appendChild(renderSkills);
 
   let yourInstitution = document.createElement('p');
-  yourInstitution.className = "input-label-dark";
+  yourInstitution.className = 'input-label-dark';
   yourInstitution.innerText = 'Your Institution';
   let renderInstitution = document.createElement('p');
   renderInstitution.className = 'user-input input-regular';
@@ -136,7 +133,7 @@ async function generateSavedDetailsForm(users){
   container.appendChild(renderInstitution);
 
   let forFun = document.createElement('p');
-  forFun.className = "input-label-dark";
+  forFun.className = 'input-label-dark';
   forFun.innerText = 'What you do for fun';
   let renderForFun = document.createElement('p');
   renderForFun.className = 'user-input input-big';
@@ -146,7 +143,7 @@ async function generateSavedDetailsForm(users){
   container.appendChild(renderForFun);
 
   let funFact = document.createElement('p');
-  funFact.className = "input-label-dark";
+  funFact.className = 'input-label-dark';
   funFact.innerText = 'A fun fact about you';
   let renderFunFact = document.createElement('p');
   renderFunFact.className = 'user-input input-big';
@@ -156,7 +153,7 @@ async function generateSavedDetailsForm(users){
   container.appendChild(renderFunFact);
 
   let whyRds = document.createElement('p');
-  whyRds.className = "input-label-dark";
+  whyRds.className = 'input-label-dark';
   whyRds.innerText = 'Why do you want to join Real Dev Squad?';
   let renderWhyRds = document.createElement('p');
   renderWhyRds.className = 'user-input input-big';
@@ -166,7 +163,7 @@ async function generateSavedDetailsForm(users){
   container.appendChild(renderWhyRds);
 
   let heardAbout = document.createElement('p');
-  heardAbout.className = "input-label-dark";
+  heardAbout.className = 'input-label-dark';
   heardAbout.innerText = 'How did you hear about us';
   let renderHeardAbout = document.createElement('p');
   renderHeardAbout.className = 'user-input input-regular';
@@ -174,19 +171,16 @@ async function generateSavedDetailsForm(users){
   renderHeardAbout.innerHTML = users.foundFrom;
   container.appendChild(heardAbout);
   container.appendChild(renderHeardAbout);
-  document.querySelector('.intro-main').appendChild(renderPage)
+  document.querySelector('.intro-main').appendChild(renderPage);
 }
 
-
 //making userSavedData object from API
-async function showSavedDetails(){
+async function showSavedDetails() {
   try {
     const id = await fetchSavedDetails();
-    const usersRequest = await makeApiCall(
-      `${BASE_URL}/users/${id}/intro`,
-    );
+    const usersRequest = await makeApiCall(`${BASE_URL}/users/${id}/intro`);
     let usersData = await usersRequest.json();
-    console.log(usersData)
+    console.log(usersData);
     let userSavedData = {
       firstName: usersData.data[0].biodata.firstName,
       lastName: usersData.data[0].biodata.lastName,
@@ -199,19 +193,16 @@ async function showSavedDetails(){
       funFact: usersData.data[0].intro.funFact,
       forFun: usersData.data[0].intro.forFun,
       whyRds: usersData.data[0].intro.whyRds,
-      foundFrom:usersData.data[0].foundFrom
+      foundFrom: usersData.data[0].foundFrom,
     };
     console.log(userSavedData);
 
     generateSavedDetailsForm(userSavedData);
-  }
-  catch (err) {
+  } catch (err) {
     console.error(err);
-  };
+  }
 }
 
-window.onload = function (){
-  showSavedDetails()
-}
-
-
+window.onload = function () {
+  showSavedDetails();
+};

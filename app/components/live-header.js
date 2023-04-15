288f7aa0ab10e1445174dabfda273321666aa17c
SanketDhabarde <sanketdhabarde13@gmail.com>
2023-04-15 12:57:30 +0530

    [Feat #435]: Replace buttons with tabs

--- app/components/live-header.js
+++ app/components/live-header.js
@@ -3,9 +3,18 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class LiveHeaderComponent extends Component {
-  @tracked isSharing = false;
+  @tracked TABS = [
+    { label: 'Screenshare', active: true },
+    { label: 'Previous Events', active: false },
+    { label: 'Real Dev Squad', active: false },
+  ];
 
-  @action toggleShare() {
-    this.isSharing = !this.isSharing;
+  @action tabHandler(e) {
+    const activeTab = e.target.textContent.trim();
+    this.TABS = this.TABS.map((tab) =>
+      tab.label === activeTab
+        ? { ...tab, active: true }
+        : { ...tab, active: false }
+    );
   }
 }

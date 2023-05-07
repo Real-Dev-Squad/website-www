e856af91aca6a3e1235ce82b82b823f82720f190
Satyam <bajpai1973satyam@gmail.com>
2023-05-08 00:35:47 +0530

    refact: fixed lint issues

--- app/routes/live.js
+++ app/routes/live.js
@@ -2,11 +2,11 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class LiveRoute extends Route {
-	@service router;
+  @service router;
 
-	beforeModel(transition) {
-		if (transition?.to?.queryParams?.dev !== 'true') {
-			this.router.transitionTo('/page-not-found');
-		}
-	}
+  beforeModel(transition) {
+    if (transition?.to?.queryParams?.dev !== 'true') {
+      this.router.transitionTo('/page-not-found');
+    }
+  }
 }
--- tests/acceptance/live-test.js
+++ tests/acceptance/live-test.js
@@ -10,10 +10,14 @@ module('Acceptance | live', function (hooks) {
     await visit('/live');
 
     assert.notEqual(currentURL(), '/live', 'url not equal to live!');
-    assert.strictEqual(currentURL(), '/page-not-found', 'url is /page-not-found');
+    assert.strictEqual(
+      currentURL(),
+      '/page-not-found',
+      'url is /page-not-found'
+    );
 
     await visit('/live?dev=true');
-    
+
     assert.strictEqual(currentURL(), '/live?dev=true', 'we are on live page!');
     assert.dom('[data-test-tabs]').exists();
   });

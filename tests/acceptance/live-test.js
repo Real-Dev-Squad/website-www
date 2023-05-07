53de152692e63d0f52edee9ebd5f1e722f6ff6f0
Satyam <bajpai1973satyam@gmail.com>
2023-05-07 18:11:17 +0530

    test: added waitFor test selector in live acceptance tests

--- tests/acceptance/live-test.js
+++ tests/acceptance/live-test.js
@@ -1,35 +1,20 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, waitFor } from '@ember/test-helpers';
 import { setupApplicationTest } from 'website-www/tests/helpers';
-import { later } from '@ember/runloop';
 
 module('Acceptance | live', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('skeleton exists!', async function (assert) {
-    assert.expect(3);
-    await visit('/live');
-
-    assert.strictEqual(currentURL(), '/live', 'We are on the live page!');
-
-    assert.dom('[data-test-skeleton]').exists();
-    assert.dom('[data-test-video]').doesNotExist();
-  });
-
-  test('video exists!', async function (assert) {
+  test('skeleton and video conditionally exists!', async function (assert) {
     assert.expect(5);
     await visit('/live');
 
-    const controller = this.owner.lookup('controller:live');
-
     assert.strictEqual(currentURL(), '/live', 'We are on the live page!');
-    assert.dom('[data-test-tabs]').exists();
+    assert.dom('[data-test-skeleton]').exists();
+    assert.dom('[data-test-video]').doesNotExist();
 
-    await new Promise((resolve) => later(resolve, 4000));
-
-    this.set('pageLoading', controller.get('isLoading'));
-
-    assert.false(this.pageLoading, 'State changed to false!');
+    await waitFor('[data-test-video]', { timeout: 4000 });
+    
     assert.dom('[data-test-video]').exists();
     assert.dom('[data-test-skeleton]').doesNotExist();
   });

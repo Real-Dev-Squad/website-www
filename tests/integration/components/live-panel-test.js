f92fbc91375a0364e6520c664e5987058f606fb1
Satyam <bajpai1973satyam@gmail.com>
2023-05-17 23:56:05 +0530

    test: added test for live-panel

--- tests/integration/components/live-panel-test.js
+++ tests/integration/components/live-panel-test.js
@@ -7,20 +7,51 @@ module('Integration | Component | live-panel', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    assert.expect(16);
+
+    const PANEL_BUTTON = [
+      {
+        id: 'screen-share',
+        srcImg: '/assets/icons/screen-share-icon.png',
+        className: 'icon-button--md',
+        alt: 'screen share button',
+      },
+      {
+        id: 'participants',
+        srcImg: '/assets/icons/people-icon.png',
+        className: 'icon-button--md',
+        alt: 'participant button',
+      },
+    ];
 
     await render(hbs`<LivePanel />`);
 
-    assert.dom(this.element).hasText('');
+    assert.dom('[data-test-live-panel]').exists();
 
-    // Template block usage:
-    await render(hbs`
-      <LivePanel>
-        template block text
-      </LivePanel>
-    `);
+    assert.dom('[data-test-button=start-live]').exists();
 
-    assert.dom(this.element).hasText('template block text');
+    PANEL_BUTTON.forEach((button) => {
+      assert.dom(`[data-test-icon-button=${button.id}]`).exists();
+      assert
+        .dom(`[data-test-icon-button=${button.id}]`)
+        .hasClass('icon-button--md');
+      assert.dom(`[data-test-iconbtn-img=${button.id}]`).exists();
+      assert.dom(`[data-test-iconbtn-img=${button.id}]`).hasAttribute('src');
+      assert.dom(`[data-test-iconbtn-img=${button.id}]`).hasAttribute('alt');
+      assert.strictEqual(
+        document
+          .querySelector(`[data-test-iconbtn-img=${button.id}]`)
+          .getAttribute('src'),
+        button.srcImg,
+        'source is same!'
+      );
+      assert.strictEqual(
+        document
+          .querySelector(`[data-test-iconbtn-img=${button.id}]`)
+          .getAttribute('alt'),
+        button.id,
+        'alt attribute is same!'
+      );
+    });
   });
 });

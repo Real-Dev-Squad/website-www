074c330802b33b721be4df9b220c2907d725e498
rohan09-raj <rajrohan1914@gmail.com>
2023-05-27 21:15:48 +0530

    embroider-setup

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const { Webpack } = require('@embroider/webpack');
+const { compatBuild } = require('@embroider/compat');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
@@ -20,5 +22,17 @@ module.exports = function (defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+  return compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticComponents: true,
+    splitAtRoutes: [
+      'route.index',
+      'route.join',
+      'route.live',
+      'route.page-not-found',
+    ],
+  });
 };

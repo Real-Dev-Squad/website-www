821de6c0934b4471b204ad59e6319ad8c5f810d8
SanketDhabarde <sanketdhabarde13@gmail.com>
2023-06-25 13:40:05 +0530

    [Test #500] Fix test

--- tests/integration/components/live-panel-test.js
+++ tests/integration/components/live-panel-test.js
@@ -8,35 +8,28 @@ module('Integration | Component | live-panel', function (hooks) {
 
   test('it renders', async function (assert) {
     assert.expect(10);
+    this.set('buttonClickHandler', () => {});
+    this.set('role', 'guest');
 
-    const PANEL_BUTTON = [
-      {
-        id: 'screen-share',
-        icon: 'material-symbols:screen-share',
-        className: 'icon-button--md',
-      },
-      {
-        id: 'participants',
-        icon: 'material-symbols:group',
-        className: 'icon-button--md',
-      },
-      {
-        id: 'start',
-        icon: 'material-symbols:arrow-forward-ios-rounded',
-        className: 'icon-button--md icon-button--pink-bg',
-      },
-    ];
-
-    await render(hbs`<LivePanel />`);
+    await render(
+      hbs`<LivePanel @buttonClickHandler={{this.buttonClickHandler}} @role={{this.role}}/>`
+    );
 
     assert.dom('[data-test-live-panel]').exists();
 
-    PANEL_BUTTON.forEach((button) => {
-      assert.dom(`[data-test-icon-button=${button.id}]`).exists();
-      assert
-        .dom(`[data-test-icon-button=${button.id}]`)
-        .hasClass('icon-button--md');
-      assert.dom(`[data-test-icon=${button.id}]`).exists();
-    });
+    assert.dom(`[data-test-icon-button=leave-room]`).exists();
+    assert
+      .dom(`[data-test-icon-button=leave-room]`)
+      .hasClass('icon-button--md');
+    assert.dom(`[data-test-icon=leave-room]`).exists();
+    assert.dom(`[data-test-icon-button=screen-share]`).doesNotExist();
+    assert.dom(`[data-test-icon-button=copy-link]`).doesNotExist();
+
+    this.set('role', 'host');
+
+    assert.dom(`[data-test-icon-button=screen-share]`).exists();
+    assert.dom(`[data-test-icon=screen-share]`).exists();
+    assert.dom(`[data-test-icon-button=copy-link]`).exists();
+    assert.dom(`[data-test-icon=copy-link]`).exists();
   });
 });

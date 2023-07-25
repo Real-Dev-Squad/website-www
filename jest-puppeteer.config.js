349485b5f3869034da89e85b706e649a523f74ad
Ravi kumar <1002kumarravi@gmail.com>
2023-07-25 20:02:52 +0530

    setup: added puppeteer for testing

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,9 @@
+const baseOptions = {
+    launch: {
+        headless: false,
+        slowMo: 300,
+        launchTimeout: 30000,
+    }
+};
+
+module.exports = baseOptions;
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const config =  {
+    preset: "jest-puppeteer",
+    collectCoverage: true,
+    collectCoverageFrom: ['js/**/*'],
+    reporters: ['default'],
+    coverageDirectory: 'coverage',
+    verbose: true
+}
+
+module.exports = config;

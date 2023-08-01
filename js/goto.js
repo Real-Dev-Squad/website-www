dbd37a0c13fce578d193080bdd093adfef95ce41
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-08-01 19:02:16 +0530

    added feature flag

--- js/goto.js
+++ js/goto.js
@@ -1,5 +1,8 @@
 import { SIGNUP_URL, HOME_URL } from './constants.js';
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
 const hasVisitedJoin = localStorage.getItem('hasVisitedJoin');
 
 function redirectUserToPage(page) {
@@ -9,13 +12,21 @@ function redirectUserToPage(page) {
 function redirectionHandler(data) {
   if (data.incompleteUserDetails) {
     redirectUserToPage(SIGNUP_URL);
-  } else if (
-    data.roles.developer == true &&
-    (hasVisitedJoin == 'true' || hasVisitedJoin == null)
-  ) {
-    redirectUserToPage(`${HOME_URL}/join`);
+  } else if (urlParams.get('dev') === 'true') {
+    if (
+      data.roles.developer == true &&
+      (hasVisitedJoin == 'true' || hasVisitedJoin == null)
+    ) {
+      redirectUserToPage(`${HOME_URL}/join.html`);
+    } else {
+      redirectUserToPage(HOME_URL);
+    }
   } else {
-    redirectUserToPage(HOME_URL);
+    if (hasVisitedJoin == 'true' || hasVisitedJoin == null) {
+      redirectUserToPage(`${HOME_URL}/join.html`);
+    } else {
+      redirectUserToPage(HOME_URL);
+    }
   }
 }
 

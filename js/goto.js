2139b7d48c11abbfe8ac25e48c164b3fe7205a70
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-08-01 11:54:52 +0530

    change in goto.js file

--- js/goto.js
+++ js/goto.js
@@ -9,7 +9,10 @@ function redirectUserToPage(page) {
 function redirectionHandler(data) {
   if (data.incompleteUserDetails) {
     redirectUserToPage(SIGNUP_URL);
-  } else if (hasVisitedJoin == 'true' || hasVisitedJoin == null) {
+  } else if (
+    data.roles.developer == true &&
+    (hasVisitedJoin == 'true' || hasVisitedJoin == null)
+  ) {
     redirectUserToPage(`${HOME_URL}/join`);
   } else {
     redirectUserToPage(HOME_URL);

20f9a1258535bdaaf2ab8db22e5e62f11fe485aa
shubham <49603826+shubhdevelop@users.noreply.github.com>
2023-08-08 13:45:26 +0530

    Fix superuser indicator turn red or green based on roles

--- js/debug.js
+++ js/debug.js
@@ -15,7 +15,7 @@ let linkedin_id = document.querySelector('.linkedin_id');
 let discordId = document.querySelector('.discordId');
 let toggleButton = document.querySelector('.toggle-button');
 let toggle = document.querySelector('.toggle');
-
+let indicator = document.querySelector('.indicator');
 let feilds = [
   ,
   profilePic,
@@ -30,6 +30,7 @@ let feilds = [
   github_id,
   linkedin_id,
   discordId,
+  indicator,
 ];
 
 const fetchUserDetails = async () => {
@@ -46,11 +47,20 @@ const fetchUserDetails = async () => {
       case 'profilePic':
         console.log(result.user.picture.url);
         feild.src = result.user.picture.url;
+        break;
       case 'all-roles':
         for (role in result.user.roles) {
           let content = `<p class="roles"><b>${role} :</b> ${result.user.roles[role]}</p>`;
           feild.innerHTML = feild.innerHTML + content;
           // insertAdjacentHTML('beforeend', '<li>third</li>')
+        }
+        break;
+      case 'indicator':
+        console.log(result.user.roles);
+        if (result.user.roles.hasOwnProperty('superuser')) {
+          feild.style.backgroundColor = ' rgb(0, 255, 8)';
+        } else {
+          feild.style.backgroundColor = 'rgb(255, 0, 43)';
         }
         break;
       default:
@@ -65,7 +75,7 @@ fetchUserDetails();
 
 let currToggleState = false;
 
-toggle.addEventListener('click', (e) => {
+function handleToggle(e) {
   if (!currToggleState) {
     currToggleState = !currToggleState;
     toggle.classList.toggle('applied');
@@ -75,4 +85,7 @@ toggle.addEventListener('click', (e) => {
     toggleButton.style.backgroundColor = 'rgb(255, 0, 43)';
     toggle.classList.toggle('applied');
   }
-});
+}
+
+//eventhandleer is on the button and not the toggle due to eventPropogation
+toggleButton.addEventListener('click', handleToggle);

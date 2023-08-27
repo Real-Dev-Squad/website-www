cd51b285107925c7e5ef2368e0498266f4de7a18
shubham <49603826+shubhdevelop@users.noreply.github.com>
2023-08-28 00:38:40 +0530

    fix: join date format

--- js/utils.js
+++ js/utils.js
@@ -32,3 +32,17 @@ function createElement({ type, classList = [], id }) {
   element.id = id ?? true;
   return element;
 }
+
+function joinDate(date) {
+  const dateObject = new Date(date);
+  const joinDate = dateObject.getDate();
+  const joinMonth = dateObject.getMonth();
+  const joinYear = dateObject.getFullYear();
+  const hour = dateObject.getHours();
+  const min = dateObject.getMinutes();
+  const sec = dateObject.getSeconds();
+  const milSec = dateObject.getMilliseconds();
+  const timezone = dateObject.toString().slice(25);
+
+  return `${joinDate}/${joinMonth}/${joinYear} ${hour}:${min}:${sec}:${milSec} ${timezone}`;
+}

1e840c500043495eff3e38632e0781e7f698418c
Vinit <vinit224488@gmail.com>
2023-08-29 06:23:26 +0530

    Remove /archived route and add conditionally rendering

--- app/routes/join.js
+++ app/routes/join.js
@@ -4,10 +4,5 @@ import { inject as service } from '@ember/service';
 export default class JoinRoute extends Route {
   @service login;
   @service router;
-
-  afterModel() {
-    if (this.login?.userData?.roles?.archived) {
-      this.router.transitionTo(`archived`);
-    }
-  }
+  @service store;
 }

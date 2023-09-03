1c74744724bf6de02e34e80173e1b426a43cdb0e
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-09-03 11:04:43 +0530

    Added test for rendering Onboarding Card during SignupStep component render

--- tests/integration/components/stepper-signup-test.js
+++ tests/integration/components/stepper-signup-test.js
@@ -6,12 +6,10 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | stepper-signup', function (hooks) {
   setupRenderingTest(hooks);
 
-  test.skip('stepper-signup page render', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('stepper-signup page render', async function (assert) {
     assert.expect(1);
-    await render(hbs`<StepperSignup />`);
 
-    assert.dom('h1').hasText('Hello world');
+    await render(hbs`<StepperSignup />`);
+    assert.dom('[data-test-onboarding-card-modal]').exists();
   });
 });

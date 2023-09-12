33913745ad5b9919708b6d4ebae0ac858d441d40
Vinit <vinit224488@gmail.com>
2023-09-12 18:28:19 +0530

    fix step zero test

--- tests/integration/components/signup-steps/step-zero-test.js
+++ tests/integration/components/signup-steps/step-zero-test.js
@@ -20,12 +20,8 @@ module('Integration | Component | signup-steps/step-zero', function (hooks) {
     this.owner.register('service:login', LoginStub);
   });
 
-  test('it renders all logos on login page', async function (assert) {
-    await render(hbs`<SignupSteps::StepZero />`);
-    assert.dom('[data-test-rds-logo]').exists();
-  });
-
   test('it renders correctly when the user is logged in', async function (assert) {
+    assert.expect(4);
     this.loginService = this.owner.lookup('service:login');
     this.loginService.loginwithgithub();
 
@@ -34,12 +30,20 @@ module('Integration | Component | signup-steps/step-zero', function (hooks) {
     await render(
       hbs`<SignupSteps::StepZero @letsGoHandler={{this.letsGoHandler}} />`
     );
-
+    assert
+      .dom('[data-test-signup-heading]')
+      .hasText('Thank you for connecting your GitHub');
+    assert
+      .dom('[data-test-signup-paragraph]')
+      .hasText(
+        'Please continue with Sign Up with in order to use features display yourself on members page etc'
+      );
     assert.dom('[data-test-rds-logo-img]').exists();
-    // assert.dom('[data-test=lets-go]').exists('data-test-lets-go');
+    assert.dom('[data-test-button=lets-go]').hasText("Let's Go");
   });
 
   test('it renders correctly when the user is not logged in', async function (assert) {
+    assert.expect(3);
     this.set('letsGoHandler', () => {});
 
     await render(
@@ -48,7 +52,8 @@ module('Integration | Component | signup-steps/step-zero', function (hooks) {
 
     assert.dom('[data-test-rds-logo]').exists();
     assert
-      .dom('.signup-page-heading')
+      .dom('[data-test-signup-heading]')
       .hasText('You are not logged in. Please log in to continue...');
+    assert.dom('[data-test-button=login]').hasText('login');
   });
 });

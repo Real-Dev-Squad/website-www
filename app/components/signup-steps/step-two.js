1cc0b89ddbc076f0efff4bb2badaa45095d75107
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-09-25 22:17:59 +0530

    add rds logo on all onboarding cards

--- app/components/signup-steps/step-two.js
+++ app/components/signup-steps/step-two.js
@@ -3,7 +3,6 @@ import Component from '@glimmer/component';
 export default class SignupStepsStepTwoComponent extends Component {
   get role() {
     const { role } = this.args;
-    console.log(role);
     return role;
   }
 }

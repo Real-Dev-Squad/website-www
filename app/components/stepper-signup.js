5d95652279a1df14e458f3118e650276bfb10fd8
Vinit <vinit224488@gmail.com>
2023-09-27 11:52:05 +0530

    fix stepper-singup js file

--- app/components/stepper-signup.js
+++ app/components/stepper-signup.js
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { action, set } from '@ember/object';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class StepperSignupComponent extends Component {
@@ -18,26 +18,6 @@ export default class StepperSignupComponent extends Component {
     this.currentStep = Number(
       new URLSearchParams(window.location.search).get('step')
     );
-  }
-
-  @tracked signupDetails = {
-    firstname: '',
-    lastname: '',
-    username: '',
-    role: {},
-  };
-
-  @action handleInputChange(key, value) {
-    if (key === 'role') {
-      this.signupDetails.role = {};
-      set(this.signupDetails.role, value, true);
-    } else {
-      set(this.signupDetails, key, value);
-    }
-  }
-
-  @action setUsername(generateUsername) {
-    this.signupDetails.username = generateUsername;
   }
 
   @action decrementStep() {

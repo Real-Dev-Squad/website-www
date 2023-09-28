049becf76a754298c26b3754831f49f02c478681
Satyam <bajpai1973satyam@gmail.com>
2023-09-28 14:24:53 +0530

    fix: ui issues in live-site

--- app/components/scroll-to-top.js
+++ app/components/scroll-to-top.js
@@ -1,7 +1,17 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
+import { tracked } from '@glimmer/tracking';
 export default class ScrollToTopComponent extends Component {
+  @service router;
+  @tracked isLive = this.router.currentRoute.name === 'live';
+
+  constructor(...args) {
+    super(...args);
+    console.log(this.isLive);
+  }
+
   @action scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }

4917a48785a1a7d11b30f95add3548d8bd03c2fe
SanketDhabarde <sanketdhabarde13@gmail.com>
2023-10-07 21:39:06 +0530

    [Refactor #685]:Made kickout modal generic

--- app/components/kickout-modal.js
+++ app/components/kickout-modal.js
@@ -11,7 +11,7 @@ export default class KickoutModalComponent extends Component {
     this.args.closeModal();
   }
 
-  @action removePeer() {
-    this.args.removePeer();
+  @action confirmFun() {
+    this.args.confirmFun();
   }
 }

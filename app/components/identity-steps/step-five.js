3f78980918eecf3c94438b53dac3ba7770be46d5
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-10-12 16:24:58 +0530

    profile-service next button connected with backend

--- app/components/identity-steps/step-five.js
+++ app/components/identity-steps/step-five.js
@@ -1,9 +1,13 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { toastNotificationTimeoutOptions } from '../../constants/toast-notification';
+import { APPS } from '../../constants/urls';
 import checkURL from '../../utils/check-url';
 
 export default class IdentityStepsStepFiveComponent extends Component {
+  @service toast;
   @tracked isMouseOnTooltip = false;
   @tracked profileURL = '';
   @tracked nextButtonDisabled = true;
@@ -26,10 +30,41 @@ export default class IdentityStepsStepFiveComponent extends Component {
     }
   }
 
-  @action handleEdit(e) {
+  @action async handleEdit(e) {
     e.preventDefault();
-    this.args.startHandler();
     this.isLoading = true;
-    this.isLoading = false;
+    try {
+      const response = await fetch(`${APPS.API_BACKEND}/users/profileURL`, {
+        method: 'PATCH',
+        body: JSON.stringify({ profileURL: this.profileURL }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
+      if (response.ok) {
+        this.toast.info(
+          'Updated profile URL!!',
+          '',
+          toastNotificationTimeoutOptions
+        );
+      } else {
+        this.toast.error(
+          'Something went wrong. Please check console errors.',
+          '',
+          toastNotificationTimeoutOptions
+        );
+      }
+    } catch (error) {
+      console.error(error);
+      this.toast.error(
+        'Something went wrong. Please check console errors.',
+        '',
+        toastNotificationTimeoutOptions
+      );
+    } finally {
+      this.isLoading = false;
+      this.args.startHandler();
+    }
   }
 }

d0d70e0d855faf74df930df47ba1de14bae11e5a
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-10-22 22:44:55 +0530

    add events test

--- tests/integration/components/events-test.js
+++ tests/integration/components/events-test.js
@@ -2,20 +2,21 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'website-www/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { EVENTS_CATEGORIES } from '../../constants/events-data';
 
 module('Integration | Component | events', function (hooks) {
   setupRenderingTest(hooks);
 
-  // TODO: add tests after events section is out of feature flag
-  test('it renders empty without feature flag', async function (assert) {
+  test('events renders', async function (assert) {
     await render(hbs`<Events />`);
 
-    assert.dom(this.element).hasText('');
-
-    await render(hbs`
-      <Events />
-    `);
-
-    assert.dom(this.element).hasText('');
+    assert.dom('[data-test-events-section]').exists();
+    assert.dom('[data-test-events-section-header]').exists();
+    for (const eventCategory in EVENTS_CATEGORIES) {
+      assert.dom(`[data-test-events-category="${eventCategory}"]`).exists();
+      EVENTS_CATEGORIES[eventCategory].forEach((event) => {
+        assert.dom(`[data-test-events-link="${event.name}"]`).exists();
+      });
+    }
   });
 });

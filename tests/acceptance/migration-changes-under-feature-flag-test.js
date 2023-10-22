186996fcef8d15d25afe73c014a7d14bd43e8618
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-10-22 22:23:03 +0530

    update migration acceptance test name, data-test attribute, use constants for events

--- tests/acceptance/migration-changes-under-feature-flag-test.js
+++ tests/acceptance/migration-changes-under-feature-flag-test.js
@@ -1,12 +1,13 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'website-www/tests/helpers';
+import { EVENTS_CATEGORIES } from '../constants/events-data';
 
 // TODO: Delete/Update tests when migration changes comes out of feature flag
 module('Acceptance | migration changes under feature flag', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('checking for changes under fearture flag', async function (assert) {
+  test('Migrated footer should exists when dev=true', async function (assert) {
     await visit('/');
 
     assert.dom('[data-events-section]').doesNotExist();
@@ -17,25 +18,16 @@ module('Acceptance | migration changes under feature flag', function (hooks) {
 
     await visit('/?dev=true');
 
-    assert.dom('[data-test-sites-title]').doesNotExist();
-    assert.dom('[data-events-section]').exists();
-    assert.dom('[data-events-section-header]').exists();
-    assert.dom('[data-events-category="Upcoming Features"]').exists();
-    assert.dom('[data-events-link="Art Feature"]').exists();
-    assert.dom('[data-events-category="Upcoming Events"]').exists();
-    assert.dom('[data-events-link="APIs made Easier"]').exists();
-    assert.dom('[data-events-category="Past Events"]').exists();
-    assert.dom('[data-events-link="Dynamic Programming"]').exists();
-    assert.dom('[data-events-link="NodeJS Workshop Part I"]').exists();
-    assert.dom('[data-events-link="Web-Mini-Conf-July-2020"]').exists();
-    assert.dom('[data-events-link="React Hooks Session"]').exists();
-    assert.dom('[data-events-link="SSR the right way"]').exists();
-    assert.dom('[data-footer-info]').exists();
-    assert.dom('[data-footer-info-members-link]').exists();
-    assert.dom('[data-footer-info-faq-link]').exists();
-    assert.dom('[data-footer-repo-text-dev]').exists();
-    assert.dom('[data-footer-repo-link-dev]').exists();
-
     assert.strictEqual(currentURL(), '/?dev=true');
+
+    assert.dom('[data-test-sites-title]').doesNotExist();
+    assert.dom('[data-test-events-section]').exists();
+    assert.dom('[data-test-events-section-header]').exists();
+    for (const eventCategory in EVENTS_CATEGORIES) {
+      assert.dom(`[data-test-events-category="${eventCategory}"]`).exists();
+      EVENTS_CATEGORIES[eventCategory].forEach((event) => {
+        assert.dom(`[data-test-events-link="${event.name}"]`).exists();
+      });
+    }
   });
 });
---
+++ tests/constants/events-data.js
@@ -0,0 +1,36 @@
+export const EVENTS_CATEGORIES = {
+  'Upcoming Features': [
+    {
+      name: 'Art Feature',
+      link: 'https://realdevsquad.com/art-feature',
+    },
+  ],
+  'Upcoming Events': [
+    {
+      name: 'APIs made Easier',
+      link: 'https://realdevsquad.com/event-APIs-made-Easier-April-2021',
+    },
+  ],
+  'Past Events': [
+    {
+      name: 'Dynamic Programming',
+      link: 'https://realdevsquad.com/event-dynamic-programming-october-2020',
+    },
+    {
+      name: 'NodeJS Workshop Part I',
+      link: 'https://realdevsquad.com/event-nodejs-workshop-october-2020',
+    },
+    {
+      name: 'Web-Mini-Conf-July-2020',
+      link: 'https://realdevsquad.com/event-web-mini-conf-july-2020',
+    },
+    {
+      name: 'React Hooks Session',
+      link: 'https://realdevsquad.com/event-react-hooks-july-2020',
+    },
+    {
+      name: 'SSR the right way',
+      link: 'https://realdevsquad.com/event-ssr-correctly-august-2020',
+    },
+  ],
+};

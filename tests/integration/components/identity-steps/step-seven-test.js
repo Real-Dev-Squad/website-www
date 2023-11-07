0102ffb535248d7c037c1fd2f1efc06c03957683
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-11-07 17:19:54 +0530

    added test for description of pending state on verification page

--- tests/integration/components/identity-steps/step-seven-test.js
+++ tests/integration/components/identity-steps/step-seven-test.js
@@ -6,21 +6,27 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | identity-steps/step-seven', function (hooks) {
   setupRenderingTest(hooks);
 
-  skip('renders heading on verification page', async function (assert) {
-    this.set('model', { profileStatus: 'BLOCKED' });
+  skip('renders heading on verification page when profile Status is pending', async function (assert) {
+    this.set('model', { profileStatus: 'PENDING' });
     await render(hbs`<IdentitySteps::StepSeven @model={{@model}} />`);
-
-    assert.dom(this.element).hasText('');
-
-    await render(hbs`
-      <IdentitySteps::StepSeven>
-        template block text
-      </IdentitySteps::StepSeven>
-    `);
 
     assert.dom('[data-test=heading]').hasClass('verification-page__heading');
     assert
       .dom('[data-test=heading]')
       .hasText('Verification process is Pending!');
+  });
+
+  skip('render description on verification page when profile Status is pending', async function (assert) {
+    this.set('model', { profileStatus: 'PENDING' });
+    await render(hbs`<IdentitySteps::StepSeven @model={{@model}} />`);
+
+    assert
+      .dom('[data-test=description]')
+      .hasClass('verification-page__description');
+    assert
+      .dom('[data-test=description]')
+      .hasText(
+        'Reload to verify and complete the linking between your Profile service and Real Dev Squad service'
+      );
   });
 });

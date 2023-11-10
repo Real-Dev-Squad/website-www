52b2e6194842220fa9e45ea8b7fd1022b01c5eac
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-11-11 01:11:28 +0530

    added button functionality of blocked page

--- app/components/identity-steps/step-seven.js
+++ app/components/identity-steps/step-seven.js
@@ -1,9 +1,11 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { PROFILE_STATUS } from '../../constants/stepper-signup-data';
+import { action } from '@ember/object';
 
 export default class IdentityStepsStepSevenComponent extends Component {
   @service login;
+  @service router;
 
   currentProfileStatus = [
     {
@@ -17,4 +19,11 @@ export default class IdentityStepsStepSevenComponent extends Component {
       icon: 'ban',
     },
   ];
+
+  @action goToGenerateChaincodePage() {
+    let currentStep = this.args.currentStep;
+    currentStep -= 3;
+    const queryParams = { dev: true, step: currentStep };
+    this.router.transitionTo('join', { queryParams });
+  }
 }

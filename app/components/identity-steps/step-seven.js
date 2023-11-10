0364f7294ef607cb0eadec1861700601766e3c89
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-11-10 13:11:16 +0530

    remove model function

--- app/components/identity-steps/step-seven.js
+++ app/components/identity-steps/step-seven.js
@@ -4,6 +4,7 @@ import { inject as service } from '@ember/service';
 
 export default class IdentityStepsStepSevenComponent extends Component {
   @service identityService;
+  @service login;
 
   @action handleRefresh() {
     this.identityService.reload();
--- app/routes/join.js
+++ app/routes/join.js
@@ -1,24 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import { APPS } from '../constants/urls';
-import { toastNotificationTimeoutOptions } from '../constants/toast-notification';
 
 export default class JoinRoute extends Route {
   @service login;
-  @service toast;
-  async model() {
-    try {
-      const response = await fetch(`${APPS.API_BACKEND}/users/self`, {
-        credentials: 'include',
-      });
-      const userData = await response.json();
-      if (response.status === 401) {
-        throw new Error('You are not logged in. Please login to continue.');
-      }
-      return userData;
-    } catch (error) {
-      console.error(error.message);
-      this.toast.error(error, '', toastNotificationTimeoutOptions);
-    }
-  }
 }

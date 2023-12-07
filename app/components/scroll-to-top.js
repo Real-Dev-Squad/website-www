de4e54a0918f160d5c93bc774c495afe3461f778
Satyam <bajpai1973satyam@gmail.com>
2023-12-07 21:09:13 +0530

    styles: fix styling of headings and scroll to top

--- app/components/scroll-to-top.js
+++ app/components/scroll-to-top.js
@@ -1,13 +1,49 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-
+import { registerDestructor } from '@ember/destroyable';
 import { tracked } from '@glimmer/tracking';
+
 export default class ScrollToTopComponent extends Component {
   @service router;
   @tracked isLive = this.router.currentRoute.name === 'live';
+  @tracked isScrollToTopVisible = false;
+
+  constructor() {
+    super(...arguments);
+
+    const onBodyScroll = myDebounce(
+      this,
+      function () {
+        if (window.scrollY !== 0) {
+          this.isScrollToTopVisible = true;
+        } else {
+          this.isScrollToTopVisible = false;
+        }
+      },
+      1000,
+    );
+
+    document.body.onscroll = onBodyScroll;
+    document.body.addEventListener('scroll', onBodyScroll);
+
+    registerDestructor(this, () =>
+      document.body.removeEventListener('scroll', onBodyScroll),
+    );
+  }
 
   @action scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
+}
+
+function myDebounce(context, callback, duration = 1000) {
+  let timeout;
+
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      callback.call(context, args);
+    }, duration);
+  };
 }

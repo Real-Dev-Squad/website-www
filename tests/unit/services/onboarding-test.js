28d8823752c2bd1bcc382b6eb05e9512afbfcdeb
Vinit <vinit224488@gmail.com>
2023-12-17 12:22:11 +0530

    refactor: add verify steps in onboarding service

--- tests/unit/services/onboarding-test.js
+++ tests/unit/services/onboarding-test.js
@@ -5,37 +5,61 @@ module('Unit | Service | onboarding', function (hooks) {
   setupTest(hooks);
 
   test('generateUsername method', async function (assert) {
+    assert.expect(4);
+
     let service = this.owner.lookup('service:onboarding');
     let store = this.owner.lookup('service:store');
 
     store.queryRecord = () =>
       Promise.resolve({
-        username: 'testuser',
+        username: 'test-user',
+        get(key) {
+          return this[key];
+        },
       });
+
+    assert.step('store.queryRecord called');
 
     let user = await service.generateUsername('Test', 'User');
 
     assert.ok(user, 'User was found');
-    assert.strictEqual(user.username, 'testuser', 'Username is correct');
+    assert.strictEqual(
+      user.get('username'),
+      'test-user',
+      'Username is correct',
+    );
+
+    assert.verifySteps(['store.queryRecord called']);
   });
 
   test('signup method', async function (assert) {
+    assert.expect(3);
+
     let service = this.owner.lookup('service:onboarding');
     let store = this.owner.lookup('service:store');
 
-    store.createRecord = () => ({
-      setProperties() {},
-      save() {
-        return Promise.resolve();
-      },
-    });
+    store.peekRecord = () => null;
+
+    store.createRecord = () => {
+      assert.step('store.createRecord called');
+      return {
+        setProperties() {},
+        save() {
+          return Promise.resolve();
+        },
+      };
+    };
 
     let dataToUpdate = {
       username: 'testuser',
-      roles: {},
+      roles: {
+        maven: true,
+      },
     };
 
     await service.signup(dataToUpdate, 'role');
     assert.ok(true, 'No errors were thrown');
+
+    assert.verifySteps(['store.createRecord called']);
   });
 });

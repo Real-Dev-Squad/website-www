0993cace02b831ad39f1cac941cc7c54582c207f
Satyam Bajpai <bajpai1973satyam@gmail.com>
2023-12-27 20:14:34 +0000

    tests: add test for event card component

--- tests/integration/components/event-card-test.js
+++ tests/integration/components/event-card-test.js
@@ -2,25 +2,42 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'website-www/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { EVENTS_PAGE_MAPPING } from '../../constants/events-data';
 
 module('Integration | Component | event-card', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    assert.expect(12);
 
-    await render(hbs`<EventCard />`);
+    this.set('event', EVENTS_PAGE_MAPPING[0]);
 
-    assert.dom().hasText('');
-
-    // Template block usage:
     await render(hbs`
-      <EventCard>
-        template block text
-      </EventCard>
-    `);
+    <EventCard 
+    @title={{this.event.title}}
+    @description={{this.event.description}}
+    @eventLink={{this.event.eventLink}}
+    @timing={{this.event.timing}}
+    @speakers={{this.event.speakers}}
+    />`);
 
-    assert.dom().hasText('template block text');
+    assert.dom('[data-test-event-card]').exists();
+
+    assert.dom('[data-test-event-card-title]').exists();
+    assert.dom('[data-test-event-card-title]').hasText(this.event.title);
+
+    assert.dom('[data-test-event-card-description]').exists();
+    assert
+      .dom('[data-test-event-card-description]')
+      .hasText(this.event.description);
+
+    assert.dom('[data-test-event-card-timing]').hasText(this.event.timing);
+
+    assert.dom('[data-test-event-card="speaker-name"]').exists();
+    assert.dom('[data-test-event-card="linkedin-logo"]').exists();
+    assert.dom('[data-test-event-card="speaker-info"]').exists();
+    assert.dom('[data-test-event-card="highlights"]').exists();
+    assert.dom('[data-test-event-card="cta-button-container"]').exists();
+    assert.dom('[data-test-event-card="button"]').exists();
   });
 });

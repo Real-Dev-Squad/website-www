affd37f053e8cb8e1f02c937d71250b4a940ee6a
Satyam <bajpai1973satyam@gmail.com>
2023-12-28 14:06:04 +0530

    tests: add initial test case for word cloud component

--- tests/integration/components/word-cloud-test.js
+++ tests/integration/components/word-cloud-test.js
@@ -1,17 +1,22 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'website-www/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | word-cloud', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    assert.ok(true);
-    // TODO - add tests
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    this.set('isWordCloud', true);
 
-    // await render(hbs`<WordCloud />`);
+    await render(hbs`<WordCloud 
+    @isWordCloud={{this.isWordCloud}}
+    />`);
 
-    // assert.dom().hasText('');
+    assert.dom('[data-test-word-cloud]').exists();
+
+    this.set('isWordCloud', false);
+
+    assert.dom('[data-test-word-cloud]').doesNotExist();
   });
 });

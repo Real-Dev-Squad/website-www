62b7f8e7c0036f96d8277cf13ea7053b87c9a67a
vinit717 <vinit224488@gmail.com>
2024-01-29 10:40:16 +0530

    change class name

--- tests/integration/components/stepper-signup-test.js
+++ tests/integration/components/stepper-signup-test.js
@@ -6,12 +6,21 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | stepper-signup', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('stepper-signup page render', async function (assert) {
-    assert.expect(2);
+  test('stepper-signup page renders without error message', async function (assert) {
+    assert.expect(1);
 
     await render(hbs`<StepperSignup />`);
 
-    assert.dom('[data-test-onboarding-card-modal]').exists();
     assert.dom('[data-test-error-message]').doesNotExist();
+  });
+
+  test('stepper-signup page renders with error message', async function (assert) {
+    assert.expect(1);
+    await render(hbs`
+      <StepperSignup />
+      <p class="error-message" data-test-error-message>Please fill in all the required fields</p>
+    `);
+
+    assert.dom('[data-test-error-message]').exists();
   });
 });

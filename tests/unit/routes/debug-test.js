8b6a51706c50cae220cba3eea6b9d75aab00155d
Ajoy Kumar Das <87164140+ajoykumardas12@users.noreply.github.com>
2024-02-07 17:04:06 +0530

    Tests For Debug Page PR #852 (#856)
    * setup:basic-debug-page
    
    * styles:tidy us styles
    
    * feat/debug-page-functionalities
    
    * add box-shadow
    
    * add fallback image
    
    * add basic modal
    
    * change modifier syntax
    
    * remove console log
    
    * skip test for current pr
    
    * responsive design
    
    * max-width on bigger screens
    
    * add feature flag to debug page
    
    * add user data load error response
    
    * using async-await for data fetching
    
    * update property name
    
    * chore:update function name
    
    * remove unnecessary toast message
    
    * tests: debug-route
    
    * tests: debug-grids
    
    * tests: isoToLocalDate
    
    * tests: remove timezone dependant test
    
    * resolve comments related to css
    
    * refactor: use login service for user data
    
    * tests: update tests now using login service
    
    * style: update BEM convention
    
    * chore: update class name in test
    
    * tests: use data from constants to assert
    
    * style: update classnames - BEM convention
    
    * feat: improved unauthenticated message
    
    * tests: update class names in tests
    
    * fix: optional chaining
    
    ---------
    
    Co-authored-by: Satyam Bajpai <bajpai1973satyam@gmail.com>

--- tests/unit/routes/debug-test.js
+++ tests/unit/routes/debug-test.js
@@ -1,5 +1,6 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'website-www/tests/helpers';
+import { visit } from '@ember/test-helpers';
 
 module('Unit | Route | debug', function (hooks) {
   setupTest(hooks);
@@ -7,5 +8,19 @@ module('Unit | Route | debug', function (hooks) {
   test('it exists', function (assert) {
     let route = this.owner.lookup('route:debug');
     assert.ok(route);
+  });
+
+  test('visiting /debug', async function (assert) {
+    await visit('/debug');
+    assert.expect(1);
+
+    assert.dom('[data-test-debug]').doesNotExist();
+  });
+
+  test('visiting /debug under feature flag', async function (assert) {
+    await visit('/debug?dev=true');
+    assert.expect(1);
+
+    assert.dom('[data-test-debug]').exists();
   });
 });

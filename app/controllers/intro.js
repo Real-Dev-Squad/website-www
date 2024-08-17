ffc66c83e81d78bb1c6a037a404bf7ed5f090817
rishi-19-04 <rishikavya19@gmail.com>
2024-08-17 23:52:58 +0530

    added approve and reject buttons with a feedback box

--- app/controllers/intro.js
+++ app/controllers/intro.js
@@ -3,4 +3,20 @@ import { inject as service } from '@ember/service';
 
 export default class IntroController extends Controller {
   @service login;
+  @service onboarding;
+
+  @action
+  async approveAction(){
+    alert('Approved')
+    const inviteLink = await this.onboarding.discordInvite();
+    if(inviteLink) {
+      window.open(`https://${inviteLink}`, '_blank');
+    }
+  }
+
+  @action
+  rejectAction(){
+    alert('Rejected')
+  }
+
 }

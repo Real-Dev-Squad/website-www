9e3d406c4b5db6e72ebf65c6b64143301b10f939
vinit717 <vinit224488@gmail.com>
2024-08-19 23:04:28 +0530

    fix: super user redirection on /intro route

--- app/routes/intro.js
+++ app/routes/intro.js
@@ -1,27 +1,49 @@
 import Route from '@ember/routing/route';
 import fetch from 'fetch';
 import { inject as service } from '@ember/service';
-import { APPLICATION_URL } from '../constants/apis';
+import { APPLICATION_URL, USER_APPLICATION_LINK } from '../constants/apis';
+import { APPS } from '../constants/urls';
 
 export default class IntroRoute extends Route {
   queryParams = {
     id: { refreshModel: true },
+    status: { refreshModel: true },
   };
 
   @service fastboot;
   @service router;
+  @service login;
 
   async model(params) {
     const userId = params.id;
-    const response = await fetch(APPLICATION_URL(userId), {
-      credentials: 'include',
-    });
+    const status = params.status;
 
-    if (response.status === 404) {
+    try {
+      const response = await fetch(APPLICATION_URL(userId), {
+        credentials: 'include',
+      });
+
+      const userResponse = await fetch(`${APPS.API_BACKEND}/users/self`, {
+        credentials: 'include',
+      });
+
+      const userData = await userResponse.json();
+
+      if (response.status === 404) {
+        this.router.transitionTo('/page-not-found');
+        return;
+      }
+
+      const data = await response.json();
+
+      if (status === 'submitted' && userData.roles?.super_user) {
+        window.location.replace(USER_APPLICATION_LINK(userId));
+      }
+
+      return data.data;
+    } catch (error) {
+      console.error('Error fetching application details:', error);
       this.router.transitionTo('/page-not-found');
     }
-
-    const data = await response.json();
-    return data.data;
   }
 }

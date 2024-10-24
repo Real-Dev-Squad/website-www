ae1a68929fe4c5b8b4c1cffeea6bf9fb69e4be1d
Lakshay Manchanda <45519620+lakshayman@users.noreply.github.com>
2024-10-25 01:53:10 +0530

    Dev to Main (Subscription Feature) (#951)
    * Subscription feature (#936)
    
    * feat: add form to collect subscription data
    
    * feat: add form to collect subscription data
    
    * WIP: add ember-phone-input
    
    * feat: add not loggedin and subscribed screen and integreate API
    
    * feat: fixing bug
    
    * fix: bug to show form even if user is already subscribed
    
    * fix: urls
    
    * fix: urls
    
    * fix: urls
    
    * delete: service subscribe test
    
    * fix: resolve comments
    
    * Update app/components/header.hbs
    
    Co-authored-by: Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
    
    * fix: comments and remove modal
    
    * fix: phone number input
    
    * fix: content change
    
    * fix: remove card
    
    * fix: remove comments
    
    * fix:subscribe message
    
    ---------
    
    Co-authored-by: Vinit khandal <111434418+vinit717@users.noreply.github.com>
    Co-authored-by: Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
    
    * subscription feature fixes (#944)
    
    * fix: navbar & css & phone validation
    
    * fix: PR comments
    
    * feat: add error to in valid input
    
    * fix: lint
    
    * rename: phoneNumber to phone (#953)
    
    ---------
    
    Co-authored-by: TEJAS <98630752+tejaskh3@users.noreply.github.com>
    Co-authored-by: Vinit khandal <111434418+vinit717@users.noreply.github.com>
    Co-authored-by: Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>

---
+++ app/controllers/subscribe.js
@@ -0,0 +1,100 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { RDS_TWITTER, APPS } from '../constants/urls';
+import { TOAST_OPTIONS } from '../constants/toast-options';
+import { phoneNumberRegex } from '../constants/regex';
+
+export default class SubscribeController extends Controller {
+  @service login;
+  @tracked isFormOpen = false;
+  @tracked email = '';
+  @tracked phone = '';
+  @tracked userData = null;
+  @tracked isLoading = false;
+  @tracked showSubscribedMessage = false;
+  @service toast;
+  @tracked isPhoneValid = true;
+
+  RDS_TWITTER = RDS_TWITTER;
+
+  constructor() {
+    super(...arguments);
+    this.userData = this.login.userData;
+  }
+
+  get isLoggedIn() {
+    return this.login.isLoggedIn;
+  }
+
+  get isSubscribed() {
+    return this.login.userData.isSubscribed;
+  }
+
+  get isSubmitDisabled() {
+    const isPhoneValid = !this.phone || phoneNumberRegex.test(this.phone);
+    return !this.email || !isPhoneValid;
+  }
+
+  @action
+  toggleFormModal() {
+    this.isFormOpen = !this.isFormOpen;
+  }
+
+  @action
+  updateEmail(event) {
+    this.email = event.target.value;
+  }
+
+  @action
+  updatePhone(event) {
+    this.phone = event.target.value;
+    this.isPhoneValid = !this.phone || phoneNumberRegex.test(this.phone);
+  }
+
+  @action
+  async handleSubmit(event) {
+    event.preventDefault();
+    if (!this.isSubmitDisabled) {
+      this.isLoading = true;
+      try {
+        const url = `${APPS.API_BACKEND}/subscription?dev=true`;
+        const response = await fetch(url, {
+          method: 'POST',
+          body: JSON.stringify({
+            email: this.email,
+            phone: this.phone,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        });
+        if (!response.ok) {
+          this.toast.error(
+            'Something went wrong. Please try again.',
+            '',
+            TOAST_OPTIONS,
+          );
+        } else {
+          this.login.userData.isSubscribed = true;
+          this.isFormOpen = false;
+          this.showSubscribedMessage = true;
+          this.toast.success('🎉 Thankyou for subscribing!', '', TOAST_OPTIONS);
+        }
+      } catch (error) {
+        console.log(error);
+        this.toast.error(
+          `Something went wrong. ${error.message}`,
+          '',
+          TOAST_OPTIONS,
+        );
+      } finally {
+        this.isLoading = false;
+      }
+      this.email = '';
+      this.phone = '';
+    }
+  }
+}

f5188d0c36f9d1debd2460902a2570d06f0d5fdd
Shubham raj <shubhamxraj10@gmail.com>
2024-10-28 22:27:42 +0530

    remove /debug page from dev feature flag (#948)
    Co-authored-by: Achintya Chatterjee <55826451+Achintya-Chatterjee@users.noreply.github.com>

--- app/routes/debug.js
+++ app/routes/debug.js
@@ -1,11 +1,2 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-
-export default class DebugRoute extends Route {
-  @service router;
-  beforeModel(transition) {
-    if (transition?.to?.queryParams?.dev !== 'true') {
-      this.router.transitionTo('/page-not-found');
-    }
-  }
-}
+export default class DebugRoute extends Route {}

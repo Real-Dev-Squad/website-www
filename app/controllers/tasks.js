f0bab120ad40937dab94cf09b14da567a6978e23
tejaskh3 <tejaskh3@gmail.com>
2024-12-01 23:48:04 +0530

    feat: add tab for tasks status

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -1,3 +1,14 @@
 import Controller from '@ember/controller';
-
-export default class TasksController extends Controller {}
+import { TASK_STATUS_LIST, TABS_TASK_STATUS_LIST } from '../constants/tasks';
+export default class TasksController extends Controller {
+  taskStatusList = TASK_STATUS_LIST;
+  tabsTaskStatusList = TABS_TASK_STATUS_LIST;
+  showDropDown = false;
+  toggleDropDown = () => {
+    this.showDropDown = !this.showDropDown;
+  };
+  changeUserSelectedTask = () => {};
+  userSelectedTaskText = '';
+  showTasks = false;
+  toggleTasks = () => {};
+}

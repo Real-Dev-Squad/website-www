4e28b4c892adcfab75361e5ed69d537748be917d
ankushdharkar <ankushdharkar@gmail.com>
2025-01-07 18:47:56 -0500

    refactor(970): added review changes: year - 2024

--- tests/integration/components/notice-for-joining-test.js
+++ tests/integration/components/notice-for-joining-test.js
@@ -7,9 +7,6 @@ module('Integration | Component | notice-for-joining', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`<NoticeForJoining />`);
     assert.dom().includesText('not accepting new');
     assert.dom().includesText('check back in 2024');

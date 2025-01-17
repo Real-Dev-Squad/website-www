ec59cf4a77a8f70b590940f0335339ee9d775d72
Mayank Bansal <mayankbansal125@gmail.com>
2025-01-18 01:56:08 +0530

    feat: add user status modal, controller for status

---
+++ app/controllers/status.js
@@ -0,0 +1,106 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { toastNotificationTimeoutOptions } from '../constants/toast-notification';
+import { USER_STATES } from '../constants/user-status';
+import { getUTCMidnightTimestampFromDate } from '../utils/date-conversion';
+import { APPS } from '../constants/urls';
+const BASE_URL = APPS.API_BACKEND;
+
+export default class StatusController extends Controller {
+  @service featureFlag;
+  @service toast;
+  @tracked status = this.model;
+  @tracked isStatusUpdating = false;
+  @tracked showUserStateModal = false;
+  @tracked newStatus;
+
+  @action toggleUserStateModal() {
+    this.showUserStateModal = !this.showUserStateModal;
+  }
+
+  @action async updateStatus(newStatus) {
+    this.isStatusUpdating = true;
+    if (!('cancelOoo' in newStatus)) {
+      if (newStatus.currentStatus.state !== USER_STATES.ACTIVE) {
+        this.toggleUserStateModal();
+      }
+    }
+    try {
+      await fetch(`${BASE_URL}/users/status/self?userStatusFlag=true`, {
+        method: 'PATCH',
+        body: JSON.stringify(newStatus),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      })
+        .then((response) => response.json())
+        .then((responseData) => {
+          if (responseData.data.currentStatus?.state) {
+            this.status = responseData.data.currentStatus.state;
+            this.toast.success(
+              'Current status updated successfully.',
+              '',
+              toastNotificationTimeoutOptions,
+            );
+          } else if (responseData.data.futureStatus?.state) {
+            this.toast.success(
+              'Future status updated successfully.',
+              '',
+              toastNotificationTimeoutOptions,
+            );
+          }
+        });
+    } catch (error) {
+      console.error('Error : ', error);
+      this.toast.error(
+        'Status Update failed. Something went wrong.',
+        '',
+        toastNotificationTimeoutOptions,
+      );
+    } finally {
+      this.isStatusUpdating = false;
+    }
+  }
+
+  @action
+  async statusUpdateDevApi(from, until, message) {
+    this.isStatusUpdating = true;
+    const statusRequestBody = {
+      type: 'OOO',
+      from: getUTCMidnightTimestampFromDate(from),
+      until: getUTCMidnightTimestampFromDate(until),
+      message,
+      state: 'PENDING',
+    };
+    try {
+      const response = await fetch(`${BASE_URL}/requests?dev=true`, {
+        method: 'POST',
+        body: JSON.stringify(statusRequestBody),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
+      if (response.ok) {
+        const data = await response.json();
+        this.toast.success(data.message, '', toastNotificationTimeoutOptions);
+      }
+    } catch (error) {
+      this.toast.error(
+        'OOO status request failed. Something went wrong.',
+        '',
+        toastNotificationTimeoutOptions,
+      );
+    } finally {
+      this.isStatusUpdating = false;
+    }
+  }
+
+  @action changeStatus(status) {
+    this.newStatus = status;
+    this.toggleUserStateModal();
+  }
+}

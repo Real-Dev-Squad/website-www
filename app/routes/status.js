5d820e01655fad1480fea8b4ea5aeaad90eb4365
Mayank Bansal <mayankbansal125@gmail.com>
2025-01-20 12:56:50 +0530

    refactor: use new toast options

--- app/routes/status.js
+++ app/routes/status.js
@@ -3,8 +3,8 @@ import { inject as service } from '@ember/service';
 import fetch from 'fetch';
 import redirectAuth from '../utils/redirect-auth';
 import { USER_STATES } from '../constants/user-status';
-import { toastNotificationTimeoutOptions } from '../constants/toast-notification';
 import { APPS } from '../constants/urls';
+import { TOAST_OPTIONS } from '../constants/toast-options';
 const BASE_URL = APPS.API_BACKEND;
 
 export default class StatusRoute extends Route {
@@ -26,21 +26,26 @@ export default class StatusRoute extends Route {
       } else if (response.status === 401) {
         this.toast.error(
           'You are not logged in. Please login to continue.',
-          '',
-          toastNotificationTimeoutOptions,
+          'Error!',
+          TOAST_OPTIONS,
         );
         // added setTimeout here because before new page opens user should be notified of error by toast
         setTimeout(redirectAuth, 2000);
       } else if (response.status === 404) {
         this.toast.error(
           "Your Status data doesn't exist yet. Please choose your status from the options below.",
-          '',
-          toastNotificationTimeoutOptions,
+          'Error!',
+          TOAST_OPTIONS,
         );
         return USER_STATES.DNE;
       }
     } catch (error) {
       console.error(error.message);
+      this.toast.error(
+        'Something went wrong. ' + error.message,
+        'Error!',
+        TOAST_OPTIONS,
+      );
     }
   }
 }

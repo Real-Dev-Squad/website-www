047d20ad3ce4f5ee627f86dac932b936056712a1
suvidh <kaushiksuvidh6@gmail.com>
2025-01-25 23:57:56 +0530

    fixed headers test

--- tests/constants/urls.js
+++ tests/constants/urls.js
@@ -4,7 +4,7 @@ export const APPS = {
   EVENTS: 'https://realdevsquad.com/events',
   MEMBERS: 'https://members.realdevsquad.com',
   STATUS: 'https://status.realdevsquad.com',
-  PROFILE: 'https://my.realdevsquad.com/profile',
+  PROFILE: 'https://www.realdevsquad.com/profile?dev=true',
   TASKS: 'https://my.realdevsquad.com/tasks',
   IDENTITY: 'https://my.realdevsquad.com/identity',
   MY_STATUS: 'https://my.realdevsquad.com',

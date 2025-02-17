fa2b5c5f2eab15ed3b67310df42bc234d2f1d8d7
Mayank Bansal <mayankbansal125@gmail.com>
2025-02-17 21:58:50 +0530

    [My Site Migration] mobile verification page (Part - 1) (#1003)
    * feat: add mobile route and unit tests
    
    * fix: use user details from test constants

---
+++ tests/unit/routes/mobile-test.js
@@ -0,0 +1,69 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'website-www/tests/helpers';
+import sinon from 'sinon';
+import { FETCH_USER_PROFILE } from 'website-www/constants/apis';
+import { nonSuperUserData } from 'website-www/tests/constants/users-data';
+
+module('Unit | Route | mobile', function (hooks) {
+  setupTest(hooks);
+
+  hooks.beforeEach(function () {
+    this.fetchStub = sinon.stub(window, 'fetch');
+    this.route = this.owner.lookup('route:mobile');
+    sinon.stub(this.route.router, 'transitionTo');
+  });
+
+  hooks.afterEach(function () {
+    this.fetchStub.restore();
+    sinon.restore();
+  });
+
+  test('redirects to 404 page if dev flag is not present', function (assert) {
+    const transition = { to: { queryParams: { dev: 'false' } } };
+
+    this.route.beforeModel(transition);
+
+    assert.ok(
+      this.route.router.transitionTo.calledOnceWith('/page-not-found'),
+      'Redirected to /page-not-found when dev is not true',
+    );
+  });
+
+  test('allows access when dev flag is true', function (assert) {
+    const transition = { to: { queryParams: { dev: 'true' } } };
+
+    this.route.beforeModel(transition);
+
+    assert.ok(
+      this.route.router.transitionTo.notCalled,
+      'No redirection occurs when dev query param is true',
+    );
+  });
+
+  test('it fetches user profile and returns userId if API responds with 200', async function (assert) {
+    const userId = nonSuperUserData.userId;
+    this.fetchStub.resolves(
+      new Response(JSON.stringify({ id: userId }), { status: 200 }),
+    );
+
+    const result = await this.route.model();
+
+    assert.ok(
+      this.fetchStub.calledOnceWith(FETCH_USER_PROFILE, {
+        credentials: 'include',
+      }),
+      'Fetch called with correct URL and options',
+    );
+    assert.strictEqual(
+      result.userId,
+      userId,
+      'Returns the correct userId from API',
+    );
+  });
+
+  test('displays error in case of 401 response', async function (assert) {
+    this.fetchStub.resolves(new Response(JSON.stringify({}), { status: 401 }));
+    const result = await this.route.model();
+    assert.strictEqual(result, undefined, 'No result returned for 401');
+  });
+});

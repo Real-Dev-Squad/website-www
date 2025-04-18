955ac75735e5161808bc8cb27e9612e2a72fe512
tejaskh3 <tejaskh3@gmail.com>
2025-04-19 01:37:19 +0530

    feat: add github workflow for test coverage

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -5,10 +5,19 @@ import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
+import {
+  forceModulesToBeLoaded,
+  sendCoverage,
+} from 'ember-cli-code-coverage/test-support';
 
 setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
 setupSinon();
+
+QUnit.done(async function () {
+  forceModulesToBeLoaded();
+  await sendCoverage();
+});
 
 start();

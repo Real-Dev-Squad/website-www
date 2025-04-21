2dc62ee4c922301cc1b0ac42d28f37bf6d06d03a
Mayank Bansal <mayankbansal125@gmail.com>
2025-04-21 20:46:35 +0530

    fix: use signupDev flag to include signup dev mode

--- app/controllers/new-signup.js
+++ app/controllers/new-signup.js
@@ -19,10 +19,10 @@ import apiRequest from '../utils/api-request';
 export default class NewSignupController extends Controller {
   @service toast;
 
-  queryParams = ['currentStep', 'dev', 'oldSignup'];
+  queryParams = ['currentStep', 'dev', 'signupDev'];
 
   @tracked dev;
-  @tracked oldSignup;
+  @tracked signupDev;
   @tracked isLoading = false;
   @tracked isButtonDisabled = true;
   @tracked error = '';
@@ -41,9 +41,9 @@ export default class NewSignupController extends Controller {
     roles: {},
   };
 
-  // replace this oldSignup flag with dev flag once new-signup page is not under dev flag
-  get useOldSignup() {
-    return this.oldSignup === 'true';
+  // replace this signupDev flag with dev flag once new-signup page is not under dev flag
+  get useSignupDev() {
+    return this.signupDev === 'true';
   }
 
   async generateUsername(firstname, lastname) {
@@ -150,7 +150,7 @@ export default class NewSignupController extends Controller {
     try {
       let user;
       this.isLoading = true;
-      if (!this.useOldSignup)
+      if (!this.useSignupDev)
         user = await this.generateUsername(
           this.signupDetails.firstName,
           this.signupDetails.lastName,
@@ -158,7 +158,7 @@ export default class NewSignupController extends Controller {
       const signupDetails = {
         first_name: this.signupDetails.firstName,
         last_name: this.signupDetails.lastName,
-        username: this.useOldSignup
+        username: this.useSignupDev
           ? this.signupDetails.username
           : user.username,
       };
@@ -178,9 +178,9 @@ export default class NewSignupController extends Controller {
         return (this.error = SIGNUP_ERROR_MESSAGES.userName);
       }
 
-      const res = this.useOldSignup
-        ? await this.registerUser(signupDetails)
-        : await this.newRegisterUser(signupDetails, roles);
+      const res = this.useSignupDev
+        ? await this.newRegisterUser(signupDetails, roles)
+        : await this.registerUser(signupDetails);
       if (res?.status === 204) {
         this.currentStep = this.LAST_STEP;
       } else {
